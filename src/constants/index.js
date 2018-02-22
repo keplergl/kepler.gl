537f34517d49f486cd4a847c83aa8d01896b3ca3
Shan He <shan@uber.com>
2018-02-22 11:19:46 -0800

    export more constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,19 @@
 
 // Constants
-export {DIMENSIONS, ALL_FIELD_TYPES, FIELD_OPTS} from './default-settings';
+export {
+  DIMENSIONS,
+  ALL_FIELD_TYPES,
+  FIELD_OPTS,
+  GEOJSON_FIELDS,
+  ICON_FIELDS,
+  TRIP_POINT_FIELDS,
+  TRIP_ARC_FIELDS,
+  SCALE_TYPES,
+  LAYER_TYPES,
+  LAYER_CLASSES,
+  LAYER_BLENDINGS,
+  AGGREGATION_TYPES,
+  MAX_DEFAULT_TOOLTIPS
+} from './default-settings';
 export {VizColorPalette, DataVizColors} from './custom-color-ranges';
 export {COLOR_RANGES, DefaultColorRange} from './color-ranges';
