b7a00731bc3559fd79b5011cda8ad3ea57616495
Ben Shope <bshope@uber.com>
2018-06-18 10:46:38 -0700

    Make requested changes

--- examples/demo-app/src/analytics.js
+++ examples/demo-app/src/analytics.js
@@ -1,52 +1,47 @@
-import { ActionTypes } from "kepler.gl/actions";
+import {ActionTypes} from 'kepler.gl/actions';
+import {LOCATION_CHANGE} from 'react-router-redux';
 
-import { LOCATION_CHANGE } from "react-router-redux";
-
-const {
-  ADD_DATA,
-  LAYER_CONFIG_CHANGE,
-  LAYER_HOVER,
-  MAP_STYLE_CHANGE,
-  UPDATE_MAP,
-  TOGGLE_MODAL,
-  TOGGLE_SIDE_PANEL
-} = ActionTypes;
-
-const getPayload = ({ payload }) => payload;
+const getPayload = ({payload}) => payload;
 
 const trackingInformation = {
-  [ADD_DATA]: ({ payload: { files } }) =>
-    files.map(({ name, size, type }) => ({ name, size, type })),
-  [LAYER_HOVER]: ({
+  [ActionTypes.LOAD_FILES]: ({files}) =>
+    files.map(({size, type}) => ({size, type})),
+  [ActionTypes.LAYER_HOVER]: ({
     payload: {
-      info: { lngLat }
+      info: {lngLat}
     }
   }) => lngLat,
-  [LAYER_CONFIG_CHANGE]: ({
+  [ActionTypes.LAYER_CONFIG_CHANGE]: ({
     payload: {
-      oldLayer: { type },
+      oldLayer: {type},
       newConfig
     }
   }) => ({
     type,
     newConfig
   }),
-  [MAP_STYLE_CHANGE]: getPayload,
-  [TOGGLE_MODAL]: getPayload,
-  [TOGGLE_SIDE_PANEL]: getPayload,
-  [UPDATE_MAP]: getPayload,
+  [ActionTypes.MAP_STYLE_CHANGE]: getPayload,
+  [ActionTypes.TOGGLE_MODAL]: getPayload,
+  [ActionTypes.TOGGLE_SIDE_PANEL]: getPayload,
+  [ActionTypes.UPDATE_MAP]: getPayload,
+  [ActionTypes.SET_FILTER]: getPayload,
   [LOCATION_CHANGE]: x => x
 };
 
 const analyticsMiddleware = store => next => action => {
   // eslint-disable-next-line no-undef
-  ga(
-    "demo_app",
-    action.type,
-    trackingInformation[action.type]
-      ? JSON.stringify(trackingInformation[action.type](action.payload, store))
-      : undefined
-  );
+  if (window && window.ga) {
+    // eslint-disable-next-line no-undef
+    window.ga(
+      'demo_app',
+      action.type,
+      trackingInformation[action.type]
+        ? JSON.stringify(
+            trackingInformation[action.type](action.payload, store)
+          )
+        : undefined
+    );
+  }
   return next(action);
 };
 
