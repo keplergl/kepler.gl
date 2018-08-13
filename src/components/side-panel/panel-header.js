37aa059a652ec3f686b82a0b8b5b9fbdcfc8e69b
Eric Socolofsky <ericsoco@uber.com>
2018-08-13 00:11:55 -0700

    [Chore] Add install-and-start script

--- src/components/side-panel/panel-header.js
+++ src/components/side-panel/panel-header.js
@@ -17,6 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
