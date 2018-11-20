7c55a731ebee3ea53551ca55c2c4228e001f55e4
Shan He <shan@uber.com>
2018-11-20 13:18:56 -0800

    fix demo-app

--- examples/demo-app/src/reducers.js
+++ examples/demo-app/src/reducers.js
@@ -34,7 +34,17 @@ import {
 } from './actions';
 
 import {DEFAULT_LOADING_METHOD, LOADING_METHODS} from './constants/default-settings';
-import {generateHashId} from 'kepler.gl/utils/utils';
+
+/**
+ * Generate a hash string based on number of character
+ * @param {number} count
+ * @returns {string} hash string
+ */
+function generateHashId(count) {
+  return Math.random()
+    .toString(36)
+    .substr(count);
+}
 
 // INITIAL_APP_STATE
 const initialAppState = {
