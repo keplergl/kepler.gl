497684e77bebb0c1d15d8db847fc1c701e8fc357
Shan He <shan@uber.com>
2019-03-07 15:22:33 -0800

    add files

---
+++ examples/replace-component/src/components/panel-toggle.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {PanelToggleFactory, Button, Icons, withState} from 'kepler.gl/components';
+import {visStateLens} from 'kepler.gl/reducers';
+
+import {setMapConfig} from '../app-reducer';
+
+const StyledPanelToggleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-right: 16px;
+  background-color: ${props => props.theme.sidePanelHeaderBg};
+`;
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 4px;
+`;
+
+const PanelToggle = PanelToggleFactory();
+
+const PanelToggleWrapper = (props) => (
+  <StyledPanelToggleWrapper>
+    <PanelToggle {...props} />
+    <ButtonWrapper>
+      <Button
+        onClick={() => props.onClickSaveConfig(props.mapState)}
+        width="120px"
+      >
+        <Icons.Files height="12px" />Save Config
+      </Button>
+    </ButtonWrapper>
+  </StyledPanelToggleWrapper>
+);
+
+const CustomPanelToggleFactory = () =>
+withState(
+  // lenses
+  [visStateLens],
+  // mapStateToProps
+  state => ({mapState: state.keplerGl.map1}),
+  {
+    onClickSaveConfig: setMapConfig
+  }
+)(PanelToggleWrapper);
+
+export default CustomPanelToggleFactory;
