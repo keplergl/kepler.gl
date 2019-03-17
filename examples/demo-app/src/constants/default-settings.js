4bd03e6aabc96049a96c3a1ac7bacc0b4304c3cb
Giuseppe Macri <gmacri@uber.com>
2019-03-17 01:10:33 +0100

    Added message for: Letting users know that data is public and can be deleted

--- examples/demo-app/src/constants/default-settings.js
+++ examples/demo-app/src/constants/default-settings.js
@@ -94,7 +94,8 @@ export const LOADING_SAMPLE_LIST_ERROR_MESSAGE = 'Not able to load sample galler
 export const LOADING_SAMPLE_ERROR_MESSAGE = 'Not able to load sample';
 export const LOADING_URL_MESSAGE = 'You can use the following formats: CSV | JSON | Kepler.gl config json. Make sure the url contains the file extension.';
 export const CORS_LINK = 'https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS';
-export const KEPLER_DISCLAIMER = '* Kepler.gl will save your map data to your personal cloud storage, only people with the URL can access your map and data.';
+export const KEPLER_DISCLAIMER = '* Kepler.gl will save your map data to your personal cloud storage, only people with the URL can access your map and data. ' +
+  'You can edit/delete the data file in your Dropbox account anytime. If you do not have a Dropbox account, please signup over here';
 
 export const DEFAULT_LOADING_METHOD = LOADING_METHODS[0];
 
