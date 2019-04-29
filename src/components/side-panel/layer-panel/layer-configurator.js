43518f22098d67cbd9c171b297819c49cf5e7f70
Shan He <shan@uber.com>
2019-04-29 15:29:18 -0700

    add id to switch and description

--- src/components/side-panel/layer-panel/layer-configurator.js
+++ src/components/side-panel/layer-panel/layer-configurator.js
@@ -39,6 +39,7 @@ import VisConfigSwitch from './vis-config-switch';
 import VisConfigSlider from './vis-config-slider';
 import LayerConfigGroup, {ConfigGroupCollapsibleContent} from './layer-config-group';
 import TextLabelPanel from './text-label-panel';
+import Switch from 'components/common/switch';
 
 import {LAYER_VIS_CONFIGS} from 'layers/layer-factory';
 
@@ -137,7 +138,6 @@ export default class LayerConfigurator extends Component {
               <VisConfigSwitch
                 {...LAYER_VIS_CONFIGS.fixedRadius}
                 {...visConfiguratorProps}
-                disabled={!layer.config.sizeField}
               />
             ) : null}
           </ConfigGroupCollapsibleContent>
