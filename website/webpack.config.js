bdf3757c401b31ee5d614ef09741707c0ebefe00
Giuseppe Macrì <gmacri@uber.com>
2019-05-22 13:50:16 -0700

    [Enhancement] Netlify webpack optimization (#525)
    * Added webpack optimization

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -139,7 +139,14 @@ const COMMON_CONFIG = {
       'DropboxClientId',
       'MapboxExportToken'
     ])
-  ]
+  ],
+
+  // Required to avoid deck.gl undefined module when code is minified
+  optimization: {
+    concatenateModules: false,
+    providedExports: false,
+    usedExports: false
+  }
 };
 
 const addDevConfig = config => {
