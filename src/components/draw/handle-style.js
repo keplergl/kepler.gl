3b205f7b83e253793ac473dd0f12472dbde252f6
Giuseppe Macri <gmacri@uber.com>
2019-07-02 16:07:49 -0700

    Added custom styles to features and handles
    Signed-off-by: Giuseppe Macri <gmacri@uber.com>

---
+++ src/components/draw/handle-style.js
@@ -0,0 +1,82 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import {RenderStates, RenderTypes} from 'react-map-gl-draw';
+import {COLORS} from './constants';
+
+const rectStyle = {
+  x: -6,
+  y: -6,
+  height: 12,
+  width: 12
+};
+
+export const DEFAULT_STATE_STYLE_STROKE = COLORS.PRIMARY;
+
+export const STATE_STYLES_STROKE = {
+  [RenderStates.SELECTED]: COLORS.PRIMARY,
+  [RenderStates.HOVERED]: COLORS.SECONDARY
+};
+
+export const DEFAULT_STATE_STYLE_FILL = '#ffffff';
+
+export const STATE_STYLES_FILL = {
+  [RenderStates.SELECTED]: COLORS.SECONDARY,
+  [RenderStates.HOVERED]: COLORS.SECONDARY,
+  [RenderStates.INACTIVE]: COLORS.PRIMARY,
+  [RenderStates.UNCOMMITTED]: COLORS.PRIMARY
+};
+
+const STATE_STYLES_STROKE_WIDTH = 2;
+
+export const RENDER_TYPE_STYLES = {
+  [RenderTypes.POINT]: state => ({
+    fill: STATE_STYLES_FILL[state],
+    stroke: state === RenderStates.SELECTED
+      ? COLORS.PRIMARY
+      : DEFAULT_STATE_STYLE_STROKE
+  }),
+  [RenderTypes.LINE_STRING]: () => rectStyle,
+  [RenderTypes.RECTANGLE]: () => rectStyle,
+  [RenderTypes.POLYGON]: () => rectStyle
+};
+
+export const DEFAULT_STATE_STYLE_OPACITY = 0;
+
+export const DEFAULT_RADIUS = 4;
+
+function noOp() {}
+
+export function getStyle({feature, state}) {
+  const style = {
+    stroke: STATE_STYLES_STROKE[state] || DEFAULT_STATE_STYLE_STROKE,
+    strokeWidth: STATE_STYLES_STROKE_WIDTH,
+    fill: DEFAULT_STATE_STYLE_FILL,
+    fillOpacity: DEFAULT_STATE_STYLE_OPACITY,
+    r: DEFAULT_RADIUS
+  };
+
+  const renderType = feature.properties ? feature.properties.renderType : feature.renderType;
+
+  return {
+    ...style,
+    ...(RENDER_TYPE_STYLES[renderType] || noOp)(state)
+  };
+}
--- src/components/map-container.js
+++ src/components/map-container.js
@@ -53,17 +53,13 @@ const StyledDraw = styled(Draw)`
   display: ${props => props.visible ? 'default' : 'none'};
 `;
 
-const getMapStyle = isEdit => {
-  return{
-    top: {
-      position: 'absolute',
-      top: '0px',
-      pointerEvents: isEdit ? 'auto' : 'none',
-      zIndex: 1
-    },
-    bottom: {
-      pointerEvents: isEdit ? 'none' : 'auto'
-    }
+const MAP_STYLE = {
+  container: {
+    display: 'inline-block',
+    position: 'relative'
+  },
+  top: {
+    position: 'absolute', top: '0px', pointerEvents: 'none'
   }
 };
 
@@ -347,7 +343,8 @@ export default function MapContainerFactory(MapPopover, MapControl) {
         layers,
         visStateActions,
         mapboxApiAccessToken,
-        mapboxApiUrl
+        mapboxApiUrl,
+        uiState
       } = this.props;
 
       let deckGlLayers = [];
@@ -373,6 +370,8 @@ export default function MapContainerFactory(MapPopover, MapControl) {
         }));
       }
 
+      const isEdit = uiState.mapControls.mapDraw.active;
+
       return (
         <DeckGL
           {...this.props.deckGlProps}
@@ -383,6 +382,7 @@ export default function MapContainerFactory(MapPopover, MapControl) {
           onBeforeRender={this._onBeforeRender}
           onHover={visStateActions.onLayerHover}
           onClick={visStateActions.onLayerClick}
+          style={{zIndex: isEdit ? -1 : 0}}
         />
       );
     }
@@ -435,12 +435,11 @@ export default function MapContainerFactory(MapPopover, MapControl) {
         onViewportChange: this._onViewportChange,
         transformRequest
       };
-      const idEdit = uiState.mapControls.mapDraw.active;
 
-      const mapContainerStyles = getMapStyle(idEdit);
+      const isEdit = uiState.mapControls.mapDraw.active;
 
       return (
-        <StyledMapContainer style={MAP_STYLE_CONTAINER}>
+        <StyledMapContainer style={MAP_STYLE.container}>
           <MapControl
             datasets={datasets}
             dragRotate={mapState.dragRotate}
@@ -458,7 +457,7 @@ export default function MapContainerFactory(MapPopover, MapControl) {
             onToggleMapControl={uiStateActions.toggleMapControl}
             onSetEditorMode={uiStateActions.setEditorMode}
           />
-          <div style={mapContainerStyles.bottom}>
+          <div>
             <MapComponent
               {...mapProps}
               key="bottom"
@@ -470,23 +469,22 @@ export default function MapContainerFactory(MapPopover, MapControl) {
             >
               {this._renderDeckOverlay(layersToRender)}
               {this._renderMapboxOverlays(layersToRender)}
+              <StyledDraw
+                editor={uiState.editor}
+                onSelect={uiStateActions.setSelectedFeature}
+                onUpdate={visStateActions.setFeatures}
+                features={visState.editor.features}
+                style={{zIndex: isEdit ? 0 : -1}}
+              />
             </MapComponent>
           </div>
-          {(mapStyle.topMapStyle || idEdit) && (
-            <div style={mapContainerStyles.top}>
+          {mapStyle.topMapStyle && (
+            <div style={MAP_STYLE.top}>
               <MapComponent
                 {...mapProps}
                 key="top"
                 mapStyle={mapStyle.topMapStyle}
-              >
-                <StyledDraw
-                  editor={uiState.editor}
-                  onSelect={uiStateActions.setSelectedFeature}
-                  onUpdate={visStateActions.setFeatures}
-                  features={visState.editor.features}
-
-                />
-              </MapComponent>
+              />
             </div>
           )}
           {this._renderMapPopover(layersToRender)}
