b192b29b876d7f6ad51731f3d81043283a87d11e
Román Jiménez <drrouman@gmail.com>
2019-07-24 17:25:55 +0200

    Add form to update visualization in kepler

--- src/components/side-panel/carto-demos.js
+++ src/components/side-panel/carto-demos.js
@@ -4,11 +4,10 @@ const demos = [
     name: 'Seattle Collisions animation',
     username: 'cartovl',
     dataset: 'seattle_collisions',
-    viz: `
-      strokeWidth: 0
-      color: ramp($addrtype, Bold)
-      filter: animation($incdate, 30, fade(1, 1))
-    `,
+    viz: `strokeWidth: 0
+color: ramp($addrtype, Bold)
+filter: animation($incdate, 30, fade(1, 1))
+`,
     mapState: {
       longitude: -122.39,
       latitude: 47.59,
@@ -21,14 +20,28 @@ const demos = [
     username: 'cartovl',
     dataset: 'cordoba_catastro',
     sql: 'select * from cordoba_catastro where year > 1900',
-    viz: `
-      strokeWidth: 0
-      color: ramp($year, purpor)
-      filter: animation($year, 20, fade(0.1, hold))
-    `,
+    viz: `strokeWidth: 0
+color: ramp($year, purpor)
+filter: animation($year, 20, fade(0.1, hold))
+`,
     mapState: {
       latitude: 37.87,
       longitude: -4.79,
+      zoom: 11
+    }
+  },
+  {
+    id: 'denver_accidents',
+    name: 'Denver accidents',
+    username: 'cartovl',
+    dataset: 'traffic_accidents',
+    viz: `width: $count/2
+color: opacity(ramp(linear($count, 0,120), RedOr), $count/20)
+strokeWidth: 0
+`,
+    mapState: {
+      latitude: 39.74961937824622,
+      longitude: -104.96505621566746,
       zoom: 11
     }
   }
--- src/components/side-panel/carto-manager.js
+++ src/components/side-panel/carto-manager.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import demos from './carto-demos';
 
+const FormElement = (component) => styled(component)`
+  width: 100%;
+  margin-bottom: 8px;
+`;
+
+const VizArea = FormElement(styled.textarea`
+  resize: vertical
+`);
+
+const Input = FormElement(styled.input`
+`);
+
+const StyledSelect = FormElement(styled.select`
+`);
+
+const Header = styled.h4`
+  color: ${props => props.theme.titleTextColor};
+`;
+
+const ErrorText = styled.p`
+  color: red;
+  font-weight: bold;
+`
+
 export default class CartoManager extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      demo: {},
+      error: null
+    };
+  }
 
   onSelectExample = (e) => {
     const value = e.target.value;
@@ -14,21 +48,105 @@ export default class CartoManager extends Component {
 
     this.props.updateMap(selectedDemo.mapState);
 
-    window.loadCartoVLMap({
-      dataset: selectedDemo.dataset,
-      query: selectedDemo.sql,
-      viz: selectedDemo.viz
+    this.updateDemo(selectedDemo);
+    this.reloadDemo(selectedDemo);
+  }
+
+  _changeUsername = (e) => {
+    const demo = this.state.demo;
+    demo.username = e.target.value;
+    
+    this.updateDemo(demo);
+    this.reloadDemo(demo);
+  }
+
+  _changeApiKey = (e) => {
+    const demo = this.state.demo;
+    demo.apiKey = e.target.value;
+    
+    this.updateDemo(demo);
+    this.reloadDemo(demo);
+  }
+
+  _changeDataset = (e) => {
+    const demo = this.state.demo;
+    demo.dataset = e.target.value;
+    
+    this.updateDemo(demo);
+    this.reloadDemo(demo);
+  }
+
+  _changeSQL = (e) => {
+    const demo = this.state.demo;
+    demo.sql = e.target.value;
+    
+    this.updateDemo(demo);
+    this.reloadDemo(demo);
+  }
+
+  _changeViz = (e) => {
+    const demo = this.state.demo;
+    demo.viz = e.target.value;
+    
+    this.updateDemo(demo);
+    this.blendViz(demo.viz);
+  }
+
+  updateDemo(demo) {
+    this.setState({ demo });
+  }
+
+  reloadDemo(demo) {
+    if (!demo.username || !(demo.dataset || demo.sql)) {
+      return;
+    }
+
+    window.loadCartoVLMap(demo);
+  }
+
+  blendViz(viz) {
+    window.blendNewViz(viz).then(() => {
+      this.setState({
+        error: null
+      });
+    })
+    .catch((error) => {
+      this.setState({
+        ...this.state,
+        error: JSON.stringify(error)
+      });
     });
   }
 
+  _getForm() {
+    return (<div>
+      <Input type='text' onChange={this._changeUsername} placeholder='CARTO username' value={this.state.demo.username || ''} />
+      <Input type='text' onChange={this._changeApiKey} placeholder='API key (defaults to default_public)' value={this.state.demo.apiKey || ''} />
+      <Input type='text' onChange={this._changeDataset} placeholder='Dataset' value={this.state.demo.dataset || ''} />
+      <Input type='text' onChange={this._changeSQL} placeholder='SQL query (overrides dataset)' value={this.state.demo.sql || ''} />
+      <VizArea rows='10' onChange={this._changeViz} placeholder='Viz string' value={this.state.demo.viz || ''}></VizArea>
+    </div>)
+  }
+
   render() {
-    return (<select onChange={this.onSelectExample}>
+    const selectBox = (<StyledSelect onChange={this.onSelectExample}>
       <option value='demo'>
         Carto visualizations
       </option>
       {demos.map((demo) => (
         <option key={demo.id} value={demo.id}>{demo.name}</option> 
       ))}
-    </select>)
+    </StyledSelect>);
+
+    const form = !this.state.demo ? null : this._getForm();
+
+    return (<div>
+      <Header>Pick an example from below</Header>
+      {selectBox}
+      {form}
+      {this.state.error && <ErrorText>
+        {this.state.error.name}:{this.state.error.type}
+      </ErrorText>}
+    </div>)
   }
 }
