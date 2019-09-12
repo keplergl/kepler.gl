a740be8c1448f7a584690919e14c3e49afb54cbb
Giuseppe Macri <gmacri@uber.com>
2019-09-12 15:06:54 +0200

    Udpated policy page content and style
    Signed-off-by: Giuseppe Macri <gmacri@uber.com>

--- website/src/components/policy.js
+++ website/src/components/policy.js
@@ -26,6 +26,8 @@ import Header from './header';
 
 const GITHUB_PROJECT = 'https://github.com/keplergl/kepler.gl';
 const GITHUB_PROJECT_ISSUES = `${GITHUB_PROJECT}/issues`;
+const MAILING_LIST_URL = 'https://groups.google.com/d/forum/kepler-gl';
+const CONTRIBUTING_URL = 'https://github.com/keplergl/kepler.gl/blob/master/contributing/CONTRIBUTING.md';
 
 const StyledContainer = styled(Container)`
   background-color: black;
@@ -33,8 +35,12 @@ const StyledContainer = styled(Container)`
   
   max-width: unset;
   
-  .kg-home__caption__description {
-    max-width: unset;
+  .description {
+    max-width: 800px
+  }
+  
+  .description-content {
+    font-weight: 500;
   }
   
   h3 {
@@ -43,6 +49,7 @@ const StyledContainer = styled(Container)`
   
   a {
     text-decoration: underline;
+    color: white;
   }
   
   a:visited {
@@ -71,20 +78,19 @@ export default class Home extends PureComponent {
                 <div className="kg-home__caption__subtitle">
                   Support Policy
                 </div>
-                <div className="kg-home__caption__description">
+                <div className="description">
                   <h3>
                     <a href="https://kepler.gl">Kepler.gl</a> is an open source project. Its source code is available at <a
                     href={GITHUB_PROJECT}>Kepler.gl Github project</a>.
                   </h3>
-                  <span>
-                  As an open source project, questions and bug reports can be filed with the project community,
-                  and you can participate in the development of the project's source code. Bug reports and
-                  issues can be submitted at <a
-                    href={GITHUB_PROJECT_ISSUES}>Kepler.gl Github issues</a>.
-                  [Questions can be asked on the project's mailing lists, which you can subscribe to at [URL] // NEED TO BE DEFINED]
-                  Please review the applicable contributing guidelines and procedures in connection with your submissions.
-                  Please note that the Kepler.gl project and Urban Computing Foundation do not otherwise provide support for users of Kepler.gl software.
-                </span>
+                  <span className="description-content">
+                    As an open source project, questions and bug reports can be filed with the project community,
+                    and you can participate in the development of the project's source code. Bug reports and
+                    issues can be submitted at <a href={GITHUB_PROJECT_ISSUES}>Kepler.gl Github issues</a>.
+                    Questions can be asked on the project's mailing lists, which you can subscribe at <a href={MAILING_LIST_URL}>Kepler.gl Mailing list</a>
+                    Please review the applicable <a href={CONTRIBUTING_URL}>contributing guidelines</a> and procedures in connection with your submissions.
+                    Please note that the Kepler.gl project and Urban Computing Foundation do not otherwise provide support for users of Kepler.gl software.
+                  </span>
                 </div>
               </StyledCaption>
             </Content>
