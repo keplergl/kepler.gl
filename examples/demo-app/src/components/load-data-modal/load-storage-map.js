dda27fc30fba9e27201362cbc670993a56d6b493
Raúl Yeguas <neokore@gmail.com>
2020-01-23 14:56:28 +0100

    Load via storage with thumbnail layout

--- examples/demo-app/src/components/load-data-modal/load-storage-map.js
+++ examples/demo-app/src/components/load-data-modal/load-storage-map.js
@@ -79,72 +79,96 @@ const StyledSeparator = styled.hr`
 
 const StyledVisualizationList = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-flow: row wrap;
   align-items: stretch;
+  justify-content: space-between;
 `;
 
 const StyledVisualizationItem = styled.div`
+  flex: 0 0 auto;
+  width: 208px;
   display: flex;
-  flex-direction: row;
-  padding: 10px 20px;
-  border: .5px solid #BFBFBF;
-  border-radius: 2px;
-  margin-bottom: 8px;
-  background-color: #FFF;
+  flex-direction: column;
+  padding: 16px 8px;
   color: #3A414C;
   cursor: pointer;
+  font-size: 12px;
+  line-height: 18px;
 
   &:hover {
-    border-color: #3A414C;
+    .vis_item-icon, .vis_item-thumb, .vis_item-description, .vis_item-modification-date {
+      opacity: 1;
+    }
   }
 
-  & > * {
-    white-space: nowrap;
+  .vis_item-icon, .vis_item-thumb, .vis_item-description, .vis_item-modification-date {
+    opacity: .9;
+    transition: opacity .4s ease;
   }
 
   .vis_item-icon {
-    flex: 0 0 auto;
+    flex: 0 0 108px;
+    background-color: #6A7484;
+    border-radius: 4px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .vis_item-thumb {
+    flex: 0 0 108px;
+    background-size: cover;
+    background-position: center;
+    border-radius: 4px; 
   }
 
   .vis_item-title {
+    margin-top: 16px;
     font-weight: 500;
-    padding-left: 8px;
-  }
-
-  .vis_item-description {
-    flex: 0 1 auto;
-    padding-left: 12px;
+    white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
   }
 
-  .vis_item-privacy {
-    flex: 0 1 auto;
-    padding-left: 12px;
+  .vis_item-description {
+    flex: 1 1 auto;
+    margin-top: 8px;
   }
 
   .vis_item-modification-date {
+    margin-top: 16px;
     flex: 1 0 auto;
-    text-align: right;
-    padding-left: 40px;
+    color: #6A7484;
+    line-height: 15px;
   }
 `;
 
 const MapIcon = (props) => {
   return (
-    <Icons.IconWrapper {...props} viewBox={"0 0 16 16"}>
-      <path fill="#d3d8d6" d="m13.6 11.572-3.2 2.1336v-9.2776l3.2-2.1336zm-12-7.144 3.2-2.1336v9.2776l-3.2 2.1336zm13.244 8.2376c0.2224-0.148 0.356-0.3984 0.356-0.6656v-11.2c0-0.2952-0.1624-0.5664-0.4224-0.7048-0.26-0.14-0.576-0.1248-0.8216 0.0392l-4.3128 2.876-3.5432-2.8352c-0.1208-0.0936-0.2952-0.1624-0.472-0.1688-0.1648-0.0064-0.348 0.0464-0.472 0.128l-4.8 3.2c-0.2224 0.1488-0.356 0.3984-0.356 0.6656v11.2c0 0.2952 0.1624 0.5664 0.4224 0.7056 0.1184 0.0632 0.248 0.0944 0.3776 0.0944 0.1552 0 0.3096-0.0448 0.444-0.1344l4.3128-2.876 3.5432 2.8352c0.1448 0.116 0.3216 0.1752 0.5 0.1752 0.1184 0 0.236-0.0248 0.3464-0.0784z"/>
-    </Icons.IconWrapper>
+    <div {...props}>
+      <Icons.IconWrapper height="32px" viewBox={"0 0 16 16"}>
+        <path fill="#d3d8d6" d="m13.6 11.572-3.2 2.1336v-9.2776l3.2-2.1336zm-12-7.144 3.2-2.1336v9.2776l-3.2 2.1336zm13.244 8.2376c0.2224-0.148 0.356-0.3984 0.356-0.6656v-11.2c0-0.2952-0.1624-0.5664-0.4224-0.7048-0.26-0.14-0.576-0.1248-0.8216 0.0392l-4.3128 2.876-3.5432-2.8352c-0.1208-0.0936-0.2952-0.1624-0.472-0.1688-0.1648-0.0064-0.348 0.0464-0.472 0.128l-4.8 3.2c-0.2224 0.1488-0.356 0.3984-0.356 0.6656v11.2c0 0.2952 0.1624 0.5664 0.4224 0.7056 0.1184 0.0632 0.248 0.0944 0.3776 0.0944 0.1552 0 0.3096-0.0448 0.444-0.1344l4.3128-2.876 3.5432 2.8352c0.1448 0.116 0.3216 0.1752 0.5 0.1752 0.1184 0 0.236-0.0248 0.3464-0.0784z"/>
+      </Icons.IconWrapper>
+    </div>
   );
 }
 
 const VisualizationItem = ({vis, onClick}) => {
   return (
     <StyledVisualizationItem onClick={onClick}>
-      <MapIcon className="vis_item-icon" height="16px"/>
+      {
+        vis.thumbnail
+        ? (<div className="vis_item-thumb" style={{backgroundImage: `url(${vis.thumbnail})`}}></div>)
+        : (<MapIcon className="vis_item-icon" />)
+      }
       <span className="vis_item-title">{vis.title}</span>
-      {vis.description && vis.description.length && (<span className="vis_item-description">{vis.description}</span>)}
-      <span className="vis_item-privacy">({vis.privateMap ? 'Private' : 'Public'})</span>
+      {
+      <span className="vis_item-description">
+          {vis.description && vis.description.length ? vis.description : ''}
+          <span className="vis_item-privacy"> ({vis.privateMap ? 'Private' : 'Public'})</span>
+      </span>
+      }
       <span className="vis_item-modification-date">Last modified {moment.utc( vis.lastModification).fromNow()}</span>
     </StyledVisualizationItem>
   );
