a246e0160d4ef44ed43c37e6c3c9d0083f9d0348
Raúl Yeguas <neokore@gmail.com>
2020-01-24 11:25:08 +0100

    Privacy label for loading maps via Storage

--- examples/demo-app/src/components/load-data-modal/load-storage-map.js
+++ examples/demo-app/src/components/load-data-modal/load-storage-map.js
@@ -107,6 +107,7 @@ const StyledVisualizationItem = styled.div`
   }
 
   .vis_item-icon {
+    position: relative;
     flex: 0 0 108px;
     background-color: #6A7484;
     border-radius: 4px;
@@ -117,10 +118,23 @@ const StyledVisualizationItem = styled.div`
   }
 
   .vis_item-thumb {
+    position: relative;
     flex: 0 0 108px;
     background-size: cover;
     background-position: center;
     border-radius: 4px; 
+  }
+
+  .vis_item-privacy {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 3px 6px;
+    border-radius: 4px 0;
+    background-color: rgba(58, 65, 76, .7);
+    color: #FFF;
+    font-size: 11px;
+    line-height: 18px;
   }
 
   .vis_item-title {
@@ -147,6 +161,7 @@ const StyledVisualizationItem = styled.div`
 const MapIcon = (props) => {
   return (
     <div {...props}>
+      {props.children}
       <Icons.IconWrapper height="32px" viewBox={"0 0 16 16"}>
         <path fill="#d3d8d6" d="m13.6 11.572-3.2 2.1336v-9.2776l3.2-2.1336zm-12-7.144 3.2-2.1336v9.2776l-3.2 2.1336zm13.244 8.2376c0.2224-0.148 0.356-0.3984 0.356-0.6656v-11.2c0-0.2952-0.1624-0.5664-0.4224-0.7048-0.26-0.14-0.576-0.1248-0.8216 0.0392l-4.3128 2.876-3.5432-2.8352c-0.1208-0.0936-0.2952-0.1624-0.472-0.1688-0.1648-0.0064-0.348 0.0464-0.472 0.128l-4.8 3.2c-0.2224 0.1488-0.356 0.3984-0.356 0.6656v11.2c0 0.2952 0.1624 0.5664 0.4224 0.7056 0.1184 0.0632 0.248 0.0944 0.3776 0.0944 0.1552 0 0.3096-0.0448 0.444-0.1344l4.3128-2.876 3.5432 2.8352c0.1448 0.116 0.3216 0.1752 0.5 0.1752 0.1184 0 0.236-0.0248 0.3464-0.0784z"/>
       </Icons.IconWrapper>
@@ -159,15 +174,16 @@ const VisualizationItem = ({vis, onClick}) => {
     <StyledVisualizationItem onClick={onClick}>
       {
         vis.thumbnail
-        ? (<div className="vis_item-thumb" style={{backgroundImage: `url(${vis.thumbnail})`}}></div>)
-        : (<MapIcon className="vis_item-icon" />)
+          ? (<div className="vis_item-thumb" style={{ backgroundImage: `url(${vis.thumbnail})` }}>
+              <span className="vis_item-privacy">{vis.privateMap ? 'Private' : 'Public'}</span>
+            </div>)
+          : (<MapIcon className="vis_item-icon">
+              <span className="vis_item-privacy">{vis.privateMap ? 'Private' : 'Public'}</span>
+            </MapIcon>)
       }
       <span className="vis_item-title">{vis.title}</span>
-      {
-      <span className="vis_item-description">
-          {vis.description && vis.description.length ? vis.description : ''}
-          <span className="vis_item-privacy"> ({vis.privateMap ? 'Private' : 'Public'})</span>
-      </span>
+      { vis.description && vis.description.length &&
+        (<span className="vis_item-description">{vis.description}</span>)
       }
       <span className="vis_item-modification-date">Last modified {moment.utc( vis.lastModification).fromNow()}</span>
     </StyledVisualizationItem>
