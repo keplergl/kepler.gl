b748cb9bf96c6f8cb9c32c9505cee00fc230f935
Shan He <heshan0131@gmail.com>
2020-01-30 11:35:08 -0800

    [Bug] should add redux devtools in demo app by default (#932)
    * should not add redux_devtools in demo app by default
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- examples/demo-app/src/store.js
+++ examples/demo-app/src/store.js
@@ -23,6 +23,7 @@ import {routerReducer, routerMiddleware} from 'react-router-redux';
 import {browserHistory} from 'react-router';
 import {enhanceReduxMiddleware} from 'kepler.gl/middleware';
 import thunk from 'redux-thunk';
+// eslint-disable-next-line no-unused-vars
 import window from 'global/window';
 
 import demoReducer from './reducers/index';
@@ -41,13 +42,23 @@ export const enhancers = [applyMiddleware(...middlewares)];
 
 const initialState = {};
 
-//  add redux devtools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-  actionsBlacklist: [
-    '@@kepler.gl/MOUSE_MOVE',
-    '@@kepler.gl/UPDATE_MAP'
-  ]
-}) || compose;
+// eslint-disable-next-line prefer-const
+let composeEnhancers = compose;
+
+/**
+ * comment out code below to enable Redux Devtools
+ */
+/*
+if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    actionsBlacklist: [
+      '@@kepler.gl/MOUSE_MOVE',
+      '@@kepler.gl/UPDATE_MAP',
+      '@@kepler.gl/LAYER_HOVER'
+    ]
+  })
+};
+*/
 
 export default createStore(
   reducers,
