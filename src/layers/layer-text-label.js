a1c6d07af41f42b5df38293bb0014714d43e5ac1
Isaac Brodsky <isaac@isaacbrodsky.com>
2020-02-19 15:35:07 -0800

    Decrease filter step size for small domains
    Otherwise the detected filter step size may be larger than the domain,
    which results in not being able to use the filter. This is applicable
    when working with small percentages (<0.1%).
    
    While changing this I also noticed this will treat negative numbers as
    very small; I changed `getNumericStepSize` to compute the step size
    based on absolute value.
    
    Signed-off-by: Isaac Brodsky <isaac@isaacbrodsky.com>

--- src/layers/layer-text-label.js
+++ src/layers/layer-text-label.js
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2020 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,6 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 import {getDistanceScales} from 'viewport-mercator-project';
 import {notNullorUndefined} from 'utils/data-utils';
 import uniq from 'lodash.uniq';
