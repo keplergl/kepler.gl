4b445b63b8c0bbf36eac4ccdbe373aa55178d4cd
Valentine <vpanch@softserveinc.com>
2020-04-29 09:29:05 +0300

    [Feat] Geocoder search fix (#1068)
    * updated tests
    
    Signed-off-by: Valentine <vpanch@softserveinc.com>

--- test/fixtures/state-saved-v1-1.js
+++ test/fixtures/state-saved-v1-1.js
@@ -873,6 +873,7 @@ mergedLayer0.config = {
       value: 'simplified_shape_v2'
     }
   },
+  hidden: false,
   isVisible: true,
   isConfigActive: false,
   highlightColor: [252, 242, 26, 255],
@@ -2709,6 +2710,7 @@ mergedLayer1.config = {
     tableFieldIndex: 13,
     analyzerType: 'FLOAT'
   },
+  hidden: false,
   sizeScale: 'linear',
   sizeDomain: [0.970877074, 1],
   textLabel: [DEFAULT_TEXT_LABEL],
