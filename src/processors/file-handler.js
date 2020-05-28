015226c550985583cd03e956656e41c0097191ad
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-05-28 09:31:08 -0300

    Install loaders.gl packages
    Parse JSON using loaders.gl
    
    Parse CSV using loaders.gl
    
    Let loaders.gl handle file detection, not working though
    
    Signed-off-by: Wesam Manassra <wesam.manassra@gmail.com>

--- src/processors/file-handler.js
+++ src/processors/file-handler.js
@@ -20,6 +20,9 @@
 
 import {FileReader} from 'global/window';
 import Console from 'global/console';
+import {parse, parseInBatches, registerLoaders} from '@loaders.gl/core';
+import {JSONLoader} from '@loaders.gl/json';
+import {CSVLoader} from '@loaders.gl/csv';
 import {
   processCsvData,
   processGeojson,
@@ -29,113 +32,131 @@ import {
 import {isPlainObject, generateHashId} from 'utils/utils';
 import {DATASET_FORMATS} from 'constants/default-settings';
 
-const FILE_HANDLERS = {
-  csv: loadCsv,
-  json: loadJSON
-};
+registerLoaders([JSONLoader, CSVLoader]);
 
-/** @type {typeof import('./file-handler').readFile} */
-export function readFile({file, fileCache = []}) {
-  return new Promise((resolve, reject) => {
-    const {handler, format} = getFileHandler(file);
-    if (!handler) {
-      Console.warn(
-        `Canont determine file handler for file ${file.name}. It must have a valid file extension`
-      );
-      resolve(fileCache);
-    }
-
-    handler({file, format}).then(result => {
-      if (!result || !result.data) {
-        // return fileCache, to keep process other files
-        resolve(fileCache);
-      }
-      resolve([
-        ...fileCache,
-        {
-          data: result.data,
-          info: {
-            label: file.name,
-            format: result.format
-          }
-        }
-      ]);
-    });
-  });
-}
-
-/** @type {typeof import('./file-handler').getFileHandler} */
-export function getFileHandler(fileBlob) {
-  const type = getFileType(fileBlob.name);
-
-  return {handler: FILE_HANDLERS[type], format: type};
-}
-
-/** @type {typeof import('./file-handler').getFileType} */
-export function getFileType(filename) {
-  if (filename.endsWith('csv')) {
-    return 'csv';
-  } else if (filename.endsWith('json') || filename.endsWith('geojson')) {
-    // Read GeoJson from browser
-    return 'json';
+export async function readFile({file, fileCache = []}) {
+  const result = await loadFile(file);
+  if (!result || !result.data) {
+    Console.warn(`Cannot parse file ${file.name}.`);
+    // return fileCache, to keep process other files
+    return fileCache;
   }
-
-  // Wait to add other file type handler
-  return 'other';
-}
-
-function readCSVFile(fileBlob) {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-    fileReader.onload = ({target: {result}}) => {
-      resolve(result);
-    };
-
-    fileReader.readAsText(fileBlob);
-  });
-}
-
-export function loadCsv({file, format, processor = processCsvData}) {
-  return readCSVFile(file).then(rawData => (rawData ? {data: processor(rawData), format} : null));
-}
-
-export function loadJSON({file, processor = processGeojson}) {
-  return readJSONFile(file).then(content => {
-    if (isKeplerGlMap(content)) {
-      return {
-        format: DATASET_FORMATS.keplergl,
-        data: processKeplerglJSON(content)
-      };
-    } else if (isRowObject(content)) {
-      return {
-        format: DATASET_FORMATS.row,
-        data: processRowObject(content)
-      };
-    } else if (isGeoJson(content)) {
-      return {
-        format: DATASET_FORMATS.geojson,
-        data: processGeojson(content)
-      };
+  return [
+    ...fileCache,
+    {
+      data: result.data,
+      info: {
+        label: file.name,
+        format: result.format
+      }
     }
-    // unsupported json format
-    Console.warn(`unsupported Json format ${file.name}`);
-    return null;
-  });
+  ];
 }
 
-export function readJSONFile(fileBlob) {
+export async function loadFile(file) {
+  // Don't read as string files with a size 250MB or bigger because it may
+  // exceed the browsers maximum string length.
+  const content =
+    file.size >= 250 * 1024 * 1024 ? await parseFileInBatches(file) : await parseFile(file);
+  if (Array.isArray(content)) {
+    return {
+      format: 'csv',
+      data: processCsvData(content)
+    };
+  } else if (isKeplerGlMap(content)) {
+    return {
+      format: DATASET_FORMATS.keplergl,
+      data: processKeplerglJSON(content)
+    };
+  } else if (isRowObject(content)) {
+    return {
+      format: DATASET_FORMATS.row,
+      data: processRowObject(content)
+    };
+  } else if (isGeoJson(content)) {
+    return {
+      format: DATASET_FORMATS.geojson,
+      data: processGeojson(content)
+    };
+  }
+  return null;
+}
+
+async function* fileReaderAsyncIterable(file, chunkSize) {
+  let offset = 0;
+  while (offset < file.size) {
+    const end = offset + chunkSize;
+    const slice = file.slice(offset, end);
+    const chunk = await new Promise((resolve, reject) => {
+      const fileReader = new FileReader(file);
+      fileReader.onload = event => {
+        resolve(event.target.result);
+      };
+      fileReader.onerror = reject;
+      fileReader.onabort = reject;
+      fileReader.readAsArrayBuffer(slice);
+    });
+    offset = end;
+    yield chunk;
+  }
+}
+
+async function parseFileInBatches(file) {
+  const chunkSize = 1024 * 1024; // 1MB, biggest value that keeps UI responsive
+  const batchIterator = await parseInBatches(fileReaderAsyncIterable(file, chunkSize), {
+    csv: {header: false},
+    json: {_rootObjectBatches: true}
+  });
+  let result = {};
+  let batches = [];
+  for await (const batch of batchIterator) {
+    // Last batch will have this special type and will provide all the root
+    // properties of the parsed document.
+    if (batch.batchType === 'root-object-batch-complete') {
+      // TODO: It would be nice if loaders.gl could handle this detail when
+      // parsing in batches, otherwise we can't entirely delegate the
+      // responsibility of parsing any format.
+      if (batch.container.features) {
+        result.features = batches;
+      } else if (batch.container.datasets) {
+        result.datasets = batches;
+      } else {
+        // HACK to get things moving, I couldn't find any realiable way to
+        // identify a Row JSON—batch.container seems to equal batches[0] though.
+        result = batches;
+      }
+      // We copy all properties but skip datasets or fatures becuase they are
+      // empty arrays—we got its content in previous batches.
+      for (const k in batch.container) {
+        if (k !== 'datasets' && k !== 'features') {
+          result[k] = batch.container[k];
+        }
+      }
+    } else {
+      batches = batches.concat(batch.data);
+    }
+  }
+  return Object.keys(result).length === 0 // csv doesn't have any keys
+    ? batches
+    : result;
+}
+
+function parseFile(file) {
   return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-    fileReader.onload = ({target: {result}}) => {
+    const fileReader = new FileReader(file);
+    fileReader.onload = async ({target: {result}}) => {
       try {
-        const json = JSON.parse(result);
-        resolve(json);
-      } catch (err) {
-        reject(null);
+        const data = await parse(result, {
+          csv: {header: false}
+        });
+        resolve(data);
+      } catch (e) {
+        reject(e);
       }
     };
-
-    fileReader.readAsText(fileBlob, 'UTF-8');
+    fileReader.onerror = reject;
+    fileReader.onabort = reject;
+    fileReader.readAsText(file, 'UTF-8');
   });
 }
 
