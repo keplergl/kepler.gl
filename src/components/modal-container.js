7a9119e6046d6a3a10d3004d8f99f23eaf1c37a1
Shan He <heshan0131@gmail.com>
2020-06-02 22:15:33 -0700

    use evenyKey and close modal when press escape key
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- src/components/modal-container.js
+++ src/components/modal-container.js
@@ -24,6 +24,7 @@ import {css} from 'styled-components';
 import {findDOMNode} from 'react-dom';
 import {createSelector} from 'reselect';
 import get from 'lodash.get';
+import document from 'global/document';
 
 import ModalDialogFactory from './modals/modal-dialog';
 import KeplerGlSchema from 'schemas';
@@ -58,7 +59,8 @@ import {
   SHARE_MAP_ID,
   OVERWRITE_MAP_ID
 } from 'constants/default-settings';
-import {EXPORT_MAP_FORMATS} from '../constants/default-settings';
+import {EXPORT_MAP_FORMATS} from 'constants/default-settings';
+import KeyEvent from 'constants/keyevent';
 
 const DataTableModalStyle = css`
   top: 80px;
@@ -139,6 +141,21 @@ export default function ModalContainerFactory(
     providerWithShare = createSelector(this.cloudProviders, cloudProviders =>
       cloudProviders.filter(p => p.hasSharingUrl())
     );
+
+    componentDidMount = () => {
+      document.addEventListener('keyup', this._onKeyUp);
+    }
+    componentWillUnmount() {
+      document.removeEventListener('keyup', this._onKeyUp);
+    }
+
+    _onKeyUp = (event) => {
+      const keyCode = event.keyCode;
+      if (keyCode === KeyEvent.DOM_VK_ESCAPE) {
+        this._closeModal();
+      }
+    }
+
     _closeModal = () => {
       this.props.uiStateActions.toggleModal(null);
     };
--- src/constants/keyevent.js
+++ src/constants/keyevent.js
@@ -23,6 +23,7 @@ import window from 'global/window';
 const KeyEvent = Object.assign({}, window.KeyEvent);
 /* eslint-enable no-use-before-define */
 
+// event event.keyCode is deprecated, should use event.key in the future
 KeyEvent.DOM_VK_UP = KeyEvent.DOM_VK_UP || 38;
 KeyEvent.DOM_VK_DOWN = KeyEvent.DOM_VK_DOWN || 40;
 KeyEvent.DOM_VK_BACK_SPACE = KeyEvent.DOM_VK_BACK_SPACE || 8;
@@ -30,5 +31,6 @@ KeyEvent.DOM_VK_RETURN = KeyEvent.DOM_VK_RETURN || 13;
 KeyEvent.DOM_VK_ENTER = KeyEvent.DOM_VK_ENTER || 14;
 KeyEvent.DOM_VK_ESCAPE = KeyEvent.DOM_VK_ESCAPE || 27;
 KeyEvent.DOM_VK_TAB = KeyEvent.DOM_VK_TAB || 9;
+KeyEvent.DOM_VK_DELETE = KeyEvent.DOM_VK_DELETE || 46;
 
 export default KeyEvent;
