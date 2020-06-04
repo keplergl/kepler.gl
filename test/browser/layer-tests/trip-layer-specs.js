182048ad3787778ccae000d9ff04c87d8104fb81
Shan He <heshan0131@gmail.com>
2020-06-04 11:13:33 -0700

    [Enhancement] filter invalid value when calculate trip layer domain (#1131)
    * filter invalid timestamp domain
    
    Signed-off-by: Shan He <heshan0131@gmail.com>
    Signed-off-by: Wesam Manassra <wesam.manassra@gmail.com>

--- test/browser/layer-tests/trip-layer-specs.js
+++ test/browser/layer-tests/trip-layer-specs.js
@@ -20,6 +20,8 @@
 
 /* eslint-disable max-statements */
 import test from 'tape';
+import cloneDeep from 'lodash.clonedeep';
+
 import TripLayer, {defaultWidth} from 'layers/trip-layer/trip-layer';
 
 import {
@@ -31,6 +33,7 @@ import {
   valueFilterDomain0,
   animationConfig
 } from 'test/helpers/layer-utils';
+import {parseTripGeoJsonTimestamp} from 'layers/trip-layer/trip-utils';
 import {TripLayerMeta, dataToFeature, dataToTimeStamp} from 'test/fixtures/trip-geojson';
 
 test('#TripLayer -> constructor', t => {
@@ -398,5 +401,24 @@ test('#TripLayer -> renderLayer', t => {
   ];
 
   testRenderLayerCases(t, TripLayer, TEST_CASES);
+  t.end();
+});
+
+test('#TripLayer -> parseTripGeoJsonTimestamp', t => {
+  // mix with illegal character
+  const dataToFeature1 = cloneDeep(dataToFeature);
+  dataToFeature1[0].geometry.coordinates[0][3] = 0;
+  dataToFeature1[1].geometry.coordinates[0][3] = NaN;
+  dataToFeature1[2].geometry.coordinates[0][3] = null;
+  dataToFeature1[3].geometry.coordinates[0][3] = undefined;
+  dataToFeature1[4].geometry.coordinates[0][3] = 'a';
+
+  dataToFeature1[0].geometry.coordinates[
+    dataToFeature1[0].geometry.coordinates.length - 1
+  ][3] = NaN;
+
+  const result = parseTripGeoJsonTimestamp(dataToFeature1);
+
+  t.deepEqual(result.animationDomain, [1565577261, 1565578836], 'should filter out illugal value')
   t.end();
 });
