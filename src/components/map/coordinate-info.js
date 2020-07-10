baea2390b1ca8313c5cdb2a25c19e704130103f5
Shan He <heshan0131@gmail.com>
2020-07-10 13:59:05 -0700

    reduce decimal
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- src/components/map/coordinate-info.js
+++ src/components/map/coordinate-info.js
@@ -26,6 +26,7 @@ import {StyledLayerName} from './layer-hover-info';
 // 6th decimal is worth up to 0.11 m
 // https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude
 const DECIMAL = 6;
+const DECIMAL_Z = 1;
 
 const CoordinateInfoFactory = () => {
   const CoordinateInfo = ({coordinate, zoom}) => (
@@ -38,8 +39,8 @@ const CoordinateInfoFactory = () => {
         <tbody>
           <tr className="row">
             <td className="row__value">{preciseRound(coordinate[1], DECIMAL)},</td>
-            <td className="row__value">{preciseRound(coordinate[0], DECIMAL)}</td>
-            <td className="row__value">{preciseRound(zoom, DECIMAL)}</td>
+            <td className="row__value">{preciseRound(coordinate[0], DECIMAL)},</td>
+            <td className="row__value">{preciseRound(zoom, DECIMAL_Z)}z</td>
           </tr>
         </tbody>
       </table>
