d5db769459d3ca3db138277c6b1750e33d6fb837
Shan He <heshan0131@gmail.com>
2020-07-30 14:12:01 -0700

    cleanup website package.json
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -22,16 +22,15 @@ const KeplerPackage = require('./package');
 
 const PRESETS = ['@babel/preset-env', '@babel/preset-react'];
 const PLUGINS = [
-  // '@babel/plugin-transform-modules-commonjs',
-  // ['@babel/plugin-proposal-decorators', {legacy: true}],
+  '@babel/plugin-transform-modules-commonjs',
   '@babel/plugin-proposal-class-properties',
   '@babel/plugin-proposal-export-namespace-from',
-  // [
-  //   '@babel/transform-runtime',
-  //   {
-  //     regenerator: true
-  //   }
-  // ],
+  [
+    '@babel/transform-runtime',
+    {
+      regenerator: true
+    }
+  ],
   [
     'module-resolver',
     {
