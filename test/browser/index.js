d8bf8f20716f9f918fbd2dc20bccc81f4dce911c
Giuseppe Macri <macri.giuseppe@gmail.com>
2020-08-12 11:48:54 +0200

    [Bug] Fixed image export bug due to mapbox attrition logo
    Signed-off-by: Giuseppe Macri <macri.giuseppe@gmail.com>

--- test/browser/index.js
+++ test/browser/index.js
@@ -17,6 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 const configure = require('enzyme').configure;
 const Adapter = require('enzyme-adapter-react-16');
 configure({adapter: new Adapter()});
