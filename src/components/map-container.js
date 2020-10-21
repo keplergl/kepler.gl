3f0f7a6ce33e87661f7cb444986396e496e119fc
Giuseppe Macrì <gmacri@uber.com>
2020-10-21 10:11:38 -0700

    [Bug] Check for valid layer pinned prop before performing comparison (#1297)
    * [Bug] Check for valid layer pinned prop before performing comparison
    
    Signed-off-by: Giuseppe Macri <macri.giuseppe@gmail.com>

--- src/components/map-container.js
+++ src/components/map-container.js
@@ -302,7 +302,7 @@ export default function MapContainerFactory(MapPopover, MapControl, Editor) {
           layersToRender,
           datasets
         });
-        if (layerHoverProp) {
+        if (layerHoverProp && layerPinnedProp) {
           layerHoverProp.primaryData = layerPinnedProp.data;
           layerHoverProp.compareType = interactionConfig.tooltip.config.compareType;
         }
