b97b58a93147126ebee82a61a32809fff9e2b60a
Shan He <heshan0131@gmail.com>
2021-03-21 01:33:06 -0700

    [Enhancement] Choose the default field to be integer if no reals are present (#1409)
    * Choose the default field to be integer for point layer if no reals are present
    
    Signed-off-by: Shan He <heshan0131@gmail.com>
    Co-authored-by: Isaac Brodsky <isaac@unfolded.ai>

--- test/node/utils/dataset-utils-test.js
+++ test/node/utils/dataset-utils-test.js
@@ -25,27 +25,57 @@ import {processCsvData} from 'processors/data-processor';
 
 import csvData from 'test/fixtures/test-layer-data';
 
+const DEFAULT_FIELD_TEST_CASES = [
+  {
+    name: 'excluded lat',
+    csv: csvData,
+    expected: 'trip_distance'
+  },
+  {
+    name: 'empty',
+    csv: 'a\na',
+    expected: null
+  },
+  {
+    name: 'integer only',
+    csv: 'a,b\na,0\na,1',
+    expected: 'b'
+  },
+  {
+    name: 'integer and real',
+    csv: 'a,b,c\na,0,0.5\na,1,0.5',
+    expected: 'c'
+  },
+  {
+    name: 'excluded real',
+    csv: 'zipcode,b,c\n0.5,0,0.5\n0.5,1,0.5',
+    expected: 'c'
+  },
+  {
+    name: 'included real',
+    csv: 'zipcode mean,b,c\n0.5,0,0.5\n0.5,1,0.5',
+    expected: 'zipcode mean'
+  },
+  {
+    name: 'included real, with inclusion ordering',
+    csv: 'zipcode mean,a metric,b,c\n0.5,0.1,0,0.5\n0.5,0.1,1,0.5',
+    expected: 'a metric'
+  }
+];
+
 test('datasetUtils.findDefaultColorField', t => {
-  const dataset = createNewDataEntry({
-    info: {id: 'taro'},
-    data: processCsvData(csvData)
-  }).taro;
+  for (const tc of DEFAULT_FIELD_TEST_CASES) {
+    const dataset = createNewDataEntry({
+      info: {id: 'taro'},
+      data: processCsvData(tc.csv)
+    }).taro;
 
-  const defaultField = findDefaultColorField(dataset);
-  // Unfortunately lat_1 is not detected as part of a field pair :(
-  t.equals(defaultField.name, 'lat_1', 'default field name is OK');
-
-  t.end();
-});
-
-test('datasetUtils.findDefaultColorField empty', t => {
-  const dataset = createNewDataEntry({
-    info: {id: 'taro'},
-    data: processCsvData('a\na')
-  }).taro;
-
-  const defaultField = findDefaultColorField(dataset);
-  t.notOk(defaultField, 'default field is null');
-
+    const defaultField = findDefaultColorField(dataset);
+    if (!tc.expected) {
+      t.notOk(defaultField, `${tc.name}: default field is null`);
+    } else {
+      t.equals(defaultField.name, tc.expected, `${tc.name}: default field name is OK`);
+    }
+  }
   t.end();
 });
