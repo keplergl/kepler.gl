310efa270430d0b7cefc09c0cae70ec7cdf1a25a
Wesam Manassra <wesam.manassra@gmail.com>
2021-06-30 09:30:35 -0700

    Hide Mapbox attribution when using non-Mapbox tiles
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/utils/src/map-style-utils/mapbox-utils.ts
+++ src/utils/src/map-style-utils/mapbox-utils.ts
@@ -18,6 +18,29 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+/**
+ * Determines whether a Map Style is using Mapbox Tiles
+ * @param {any} mapStyle the mapStyle to check
+ * @returns true if the style is using Mapbox tiles
+ */
+export function isStyleUsingMapboxTiles(mapStyle) {
+  const sources = mapStyle.stylesheet?.sources || {};
+
+  return Object.keys(sources).some(sourceId => {
+    const {url, tiles} = sources[sourceId] || {};
+
+    if (url) {
+      return url.toLowerCase().startsWith('mapbox://');
+    }
+
+    if (tiles) {
+      return tiles.some(tileUrl => tileUrl.toLowerCase().startsWith('mapbox://'));
+    }
+
+    return false;
+  });
+}
+
 export const transformRequest = (url: string): {url: string} => {
   const isMapboxRequest =
     url.slice(8, 22) === 'api.mapbox.com' || url.slice(10, 26) === 'tiles.mapbox.com';
