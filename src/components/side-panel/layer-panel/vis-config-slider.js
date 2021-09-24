9a89ab9ca17ee89c2c9ccedb77492a91f0542a02
morphoix <morphoix@gmail.com>
2021-09-24 19:09:19 +0300

    [Feat] add custom value to layer slider
    Signed-off-by: morphoix <morphoix@gmail.com>

--- src/components/side-panel/layer-panel/vis-config-slider.js
+++ src/components/side-panel/layer-panel/vis-config-slider.js
@@ -18,12 +18,47 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import {PanelLabel, SidePanelSection} from 'components/common/styled-components';
 
 import RangeSliderFactory from 'components/common/range-slider';
 import {FormattedMessage} from 'localization';
+import {Checkbox} from 'components';
+
+const InputWrapper = styled.div`
+  display: flex;
+  line-height: 12px;
+  margin-bottom: 12px;
+`;
+
+const CustomInputWrapper = styled.div`
+  display: flex;
+`;
+
+const CustomInputLabel = styled.label`
+  color: ${props => props.theme.textColor};
+  font-weight: 500;
+  letter-spacing: 0.2px;
+  font-size: ${props => props.theme.layerConfigGroupLabelLabelFontSize};
+  padding-right: 15px;
+
+  &:last-child {
+    position: absolute;
+    right: 0;
+    padding: 0;
+  }
+`;
+
+const RangeInput = styled.input`
+  ${props => props.theme.input};
+  font-size: ${props => props.theme.sliderInputFontSize};
+  width: 44px;
+  overflow: auto;
+  height: 20px;
+  margin-top: 5px;
+`;
 
 const propTypes = {
   layer: PropTypes.object.isRequired,
@@ -50,31 +85,124 @@ export default function VisConfigSliderFactory(RangeSlider) {
     disabled,
     onChange,
     inputTheme
-  }) => (
-    <SidePanelSection disabled={Boolean(disabled)}>
-      {label ? (
-        <PanelLabel>
-          {typeof label === 'string' ? (
-            <FormattedMessage id={label} />
-          ) : typeof label === 'function' ? (
-            <FormattedMessage id={label(config)} />
+  }) => {
+    const [custom, setCustom] = useState(false);
+    const [values, setValues] = useState({
+      value: '',
+      value0: '',
+      value1: ''
+    });
+
+    const updateField = e => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+
+    const onKeyPress = useCallback(
+      e => {
+        if (e.key === 'Enter') {
+          const val = e.target.value;
+          switch (e.target.name) {
+            case 'value':
+              config.visConfig[property] = range[1] = Number(val);
+              break;
+            case 'value0':
+              config.visConfig.radiusRange[0] = range[0] = Number(val);
+              break;
+            case 'value1':
+              config.visConfig.radiusRange[1] = range[1] = Number(val);
+              break;
+            default:
+              break;
+          }
+        }
+      },
+      [config.visConfig, property, range]
+    );
+
+    const renderCustomInput = () => {
+      return (
+        <CustomInputWrapper>
+          {isRanged ? (
+            <InputWrapper>
+              <CustomInputLabel>
+                min
+                <RangeInput
+                  type="number"
+                  name="value0"
+                  value={values.value0}
+                  onChange={updateField}
+                  onKeyPress={onKeyPress}
+                />
+              </CustomInputLabel>
+              <CustomInputLabel>
+                max
+                <RangeInput
+                  type="number"
+                  name="value1"
+                  value={values.value1}
+                  onChange={updateField}
+                  onKeyPress={onKeyPress}
+                />
+              </CustomInputLabel>
+            </InputWrapper>
           ) : (
-            <FormattedMessage id={`property.${property}`} />
+            <InputWrapper>
+              <RangeInput
+                type="number"
+                name="value"
+                value={values.value}
+                onChange={updateField}
+                onKeyPress={onKeyPress}
+              />
+            </InputWrapper>
           )}
-        </PanelLabel>
-      ) : null}
-      <RangeSlider
-        range={range}
-        value0={isRanged ? config.visConfig[property][0] : range[0]}
-        value1={isRanged ? config.visConfig[property][1] : config.visConfig[property]}
-        step={step}
-        isRanged={Boolean(isRanged)}
-        onChange={value => onChange({[property]: isRanged ? value : value[1]})}
-        inputTheme={inputTheme}
-        showInput
-      />
-    </SidePanelSection>
-  );
+        </CustomInputWrapper>
+      );
+    };
+
+    return (
+      <SidePanelSection disabled={Boolean(disabled)}>
+        {label ? (
+          <PanelLabel>
+            {typeof label === 'string' ? (
+              <FormattedMessage id={label} />
+            ) : typeof label === 'function' ? (
+              <FormattedMessage id={label(config)} />
+            ) : (
+              <FormattedMessage id={`property.${property}`} />
+            )}
+          </PanelLabel>
+        ) : null}
+
+        <InputWrapper>
+          <CustomInputLabel>custom input</CustomInputLabel>
+          <Checkbox
+            id={`property.${property}`}
+            checked={custom}
+            onChange={() => setCustom(!custom)}
+          />
+        </InputWrapper>
+
+        {!custom ? (
+          <RangeSlider
+            range={range}
+            value0={isRanged ? config.visConfig[property][0] : range[0]}
+            value1={isRanged ? config.visConfig[property][1] : config.visConfig[property]}
+            step={step}
+            isRanged={Boolean(isRanged)}
+            onChange={value => onChange({[property]: isRanged ? value : value[1]})}
+            inputTheme={inputTheme}
+            showInput
+          />
+        ) : (
+          renderCustomInput()
+        )}
+      </SidePanelSection>
+    );
+  };
 
   VisConfigSlider.propTypes = propTypes;
 
