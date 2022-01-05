26eb97c1ba9c63c521c7b9606408a3f5c02aff22
hodoje <nkaraklic@foursquare.com>
2022-01-05 14:08:15 +0100

    add additional component tests
    Signed-off-by: hodoje <nkaraklic@foursquare.com>

--- test/browser/components/side-panel/layer-list-test.js
+++ test/browser/components/side-panel/layer-list-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import test from 'tape';
 
-import {StateWH3Layer} from 'test/helpers/mock-state';
+import {StateWMultiH3Layers} from 'test/helpers/mock-state';
 
 import * as VisStateActions from 'actions/vis-state-actions';
 import * as UIStateActions from 'actions/ui-state-actions';
@@ -14,10 +14,10 @@ import LayerListFactory from 'components/side-panel/layer-panel/layer-list';
 const LayerList = appInjector.get(LayerListFactory);
 
 const defaultProps = {
-  datasets: StateWH3Layer.visState.datasets,
-  layerClasses: StateWH3Layer.visState.layerClasses,
-  layerOrder: StateWH3Layer.visState.layerOrder,
-  layers: StateWH3Layer.visState.layers,
+  datasets: StateWMultiH3Layers.visState.datasets,
+  layerClasses: StateWMultiH3Layers.visState.layerClasses,
+  layerOrder: StateWMultiH3Layers.visState.layerOrder,
+  layers: StateWMultiH3Layers.visState.layers,
   uiStateActions: UIStateActions,
   visStateActions: VisStateActions
 };
@@ -32,10 +32,22 @@ test('Components -> SidePanel -> LayerPanel -> LayerList -> render sortable list
     );
   }, 'LayerList should render');
 
-  t.equal(wrapper.find('.sortable-layer-items').length, 3, 'should render 3 sortable items');
+  t.equal(wrapper.find('.sortable-layer-items').length, 6, 'should render 6 sortable items');
+
+  const titles = [];
+  const expectedTitles = [
+    'H3 Hexagon 1',
+    'H3 Hexagon 1',
+    'H3 Hexagon 1',
+    'H3 Hexagon 2',
+    'H3 Hexagon 2',
+    'H3 Hexagon 2'
+  ];
+  wrapper.find('.layer__title__editor').forEach(item => titles.push(item.getDOMNode().value));
+  t.deepEqual(titles, expectedTitles, 'should render panels in correct order');
 
   const layers = wrapper.find('.layer-panel');
-  t.equal(layers.length, 3, 'should render 3 layer panels');
+  t.equal(layers.length, 6, 'should render 6 layer panels');
 
   t.end();
 });
@@ -52,8 +64,20 @@ test('Components -> SidePanel -> LayerPanel -> LayerList -> render non-sortable 
 
   t.equal(wrapper.find('.sortable-layer-items').length, 0, 'should not render sortable items');
 
+  const titles = [];
+  const expectedTitles = [
+    'H3 Hexagon 1',
+    'H3 Hexagon 1',
+    'H3 Hexagon 1',
+    'H3 Hexagon 2',
+    'H3 Hexagon 2',
+    'H3 Hexagon 2'
+  ];
+  wrapper.find('.layer__title__editor').forEach(item => titles.push(item.getDOMNode().value));
+  t.deepEqual(titles, expectedTitles, 'should render panels in correct order');
+
   const layers = wrapper.find('.layer-panel');
-  t.equal(layers.length, 3, 'should render 3 layer panels');
+  t.equal(layers.length, 6, 'should render 6 layer panels');
 
   t.end();
 });
