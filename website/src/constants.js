842b667c0321cdec124ef07b00477940e86b7751
Yassin Siouda <yassin@siouda.com>
2022-01-27 11:34:05 +0100

    revert constants.js

--- website/src/constants.js
+++ website/src/constants.js
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export const CLOUDFRONT = 'https://raw.githubusercontent.com/datatlas-erasme/front/master/src/src/static';
+export const CLOUDFRONT = 'https://d1a3f4spazzrp4.cloudfront.net';
 export const KEPLER_GL_BUCKET = 'kepler.gl';
 export const WEBSITE_ASSET_FOLDER = 'website';
 export const DEMO_LINK = '/demo';
