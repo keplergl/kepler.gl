283586d0f3c6febd479517a1c857c8a3bd780bab
dariaterekhovaae <98411986+dariaterekhovaae@users.noreply.github.com>
2022-02-08 21:41:43 +0300

    [Feat]: Technical: Translate connect to typescript (#1700)
    * Test configuration fix
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>
    
    * Connect updated
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>

--- src/connect/with-local-selector.d.ts
+++
@@ -1,8 +0,0 @@
-import {ComponentType} from 'react';
-import KeplerGlContext from 'components/context';
-
-interface ContextSelector {
-  (state: any, context: any): KeplerGlContext;
-}
-
-export default function withLocalSelector(ParentComponent: ComponentType): ComponentType;
