fbe626be44fc13be2cb99e5495319fffbd45054e
Maksim Suslov <maksim.suslov@actionengine.com>
2022-02-21 09:40:11 +0300

    [Feat]: Technical: Translate redusers (composers and combined-updaters) to typescript (#1711)
    * Reanamed js files
    * Moved types from d.ts files
    
    Signed-off-by: Maksim Suslov <maksim.suslov@actionengine.com>

--- src/reducers/composer-helpers.ts
+++ src/reducers/composer-helpers.ts
@@ -22,49 +22,54 @@ import Console from 'global/console';
 
 const identity = state => state;
 
-/* eslint-disable no-unused-vars */
-// @ts-ignore
-export function log(text) {
+/** Returns a function that logs a value with a given message */
+export function log(text: string): (value: any) => void {
   return value => Console.log(text, value);
 }
-/* eslint-enable no-unused-vars */
-
-export function payload_(p) {
-  return {payload: p};
+/** Wraps a value in an object and stores it the `payload` field */
+export function payload_<P>(payload: P) {
+  return {payload};
 }
-
-export function apply_(updater, payload) {
+/** Wraps a value in an object and stores it the `payload` field */
+export function apply_<State, P>(
+  updater: (state: State, payload: P) => State,
+  payload: P
+): (state: State) => State {
   return state => updater(state, payload);
 }
 
-export function with_(fn) {
+export function with_<State>(
+  fn: (state: State) => (state: State) => State
+): (state: State) => State {
   return state => fn(state)(state);
 }
 
-export function if_(pred, fn) {
+export function if_<State>(pred: boolean, fn: (state: State) => State): (state: State) => State {
   return pred ? fn : identity;
 }
 
-export function compose_(fns) {
+export function compose_<State>(fns: Array<(s: State) => State>): (s: State) => State {
   return state => fns.reduce((state2, fn) => fn(state2), state);
 }
-
-export function merge_(obj) {
+/** Returns a reducer function that merges props with state */
+export function merge_<Props>(obj: Props): <State>(state: State) => State {
   return state => ({...state, ...obj});
 }
 
-export function pick_(prop) {
+export function pick_<Prop extends string>(
+  prop: Prop
+): <Value>(fn: (p: Value) => Value) => <State extends Record<Prop, Value>>(state: State) => State {
   return fn => state => ({...state, [prop]: fn(state[prop])});
 }
 
-export function swap_(item) {
+export function swap_<X extends {id: string}>(item: X): (arr: X[]) => X[] {
   return arr => arr.map(a => (a.id === item.id ? item : a));
 }
 
-export function findById(id) {
+export function findById<X extends {id: string}>(id: string): (arr: X[]) => X | undefined {
   return arr => arr.find(a => a.id === id);
 }
 
-export function map_(fn) {
+export function map_<X>(fn: (state: X) => X): (arr: X[]) => X[] {
   return arr => arr.map(e => fn(e));
 }
