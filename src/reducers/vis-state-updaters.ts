4b8f5fa563da411e69d50c4ece29e65c747d58a9
natsuapo <xiatianqi@csis.u-tokyo.ac.jp>
2022-03-14 21:25:58 +0900

    fix trip layer bugs

--- src/reducers/vis-state-updaters.ts
+++ src/reducers/vis-state-updaters.ts
@@ -573,11 +573,18 @@ export function layerConfigChangeUpdater(
     newState = updateStateOnLayerVisibilityChange(state, newLayer);
   }
 
-  return updateStateWithLayerAndData(newState, {
+  newState = updateStateWithLayerAndData(newState, {
     layer: newLayer,
     layerData,
     idx
   });
+
+  if (newLayer.config.animation.enabled) {
+    newState = updateAnimationDomain(newState);
+  }
+
+  return newState
+
 }
 
 function addOrRemoveTextLabels(newFields, textLabel) {
