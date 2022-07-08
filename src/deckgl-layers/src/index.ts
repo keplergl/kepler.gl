1fc175f9982838102a5e7d9e3889bfcf2a1ffaf8
Daria Terekhova <daria.terekhova@actionengine.com>
2022-07-08 15:59:25 +0300

    Removed unused imports
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>

--- src/deckgl-layers/src/index.ts
+++ src/deckgl-layers/src/index.ts
@@ -1,6 +1,4 @@
 export {default as ThreeDBuildingLayer} from './3d-building-layer/3d-building-layer';
-export * from './3d-building-layer/3d-building-utils';
-export * from './3d-building-layer/types';
 
 export {default as DeckGLClusterLayer} from './cluster-layer/cluster-layer';
 
@@ -9,12 +7,10 @@ export {default as EnhancedColumnLayer} from './column-layer/enhanced-column-lay
 export {default as EnhancedGridLayer} from './grid-layer/enhanced-cpu-grid-layer';
 
 export {default as EnhancedHexagonLayer} from './hexagon-layer/enhanced-hexagon-layer';
+export {default as EnhancedLineLayer} from './line-layer/line-layer';
+export {default as SvgIconLayer} from './svg-icon-layer/svg-icon-layer';
 
-export * from './layer-utils/cluster-utils';
-export * from './layer-utils/cpu-aggregator';
 export * from './layer-utils/shader-utils';
 
-export {default as EnhancedLineLayer} from './line-layer/line-layer';
-
-export * from './svg-icon-layer/scatterplot-icon-layer';
-export {default as SvgIconLayer} from './svg-icon-layer/svg-icon-layer';
+export * from './3d-building-layer/types';
+export * from './3d-building-layer/3d-building-utils';
