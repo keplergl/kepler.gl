88039cd3230ebb204e3e01dd117005c9d131f5ff
dariaterekhovaae <98411986+dariaterekhovaae@users.noreply.github.com>
2022-08-23 18:13:08 +0300

    [Chore]: Technical: Isolate cloud-providers  (#1942)
    * cloud-provider isolation
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>
    
    * package.json update
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>
    
    * examples imports updated
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>
    
    * imports merge
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>
    
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>

--- src/components/modals/status-panel.tsx
+++ src/components/modals/status-panel.tsx
@@ -24,7 +24,7 @@ import {MapIcon} from 'components/common/icons';
 import {StyledExportSection} from 'components/common/styled-components';
 import ErrorDisplay from './error-display';
 import {FormattedMessage} from '@kepler.gl/localization';
-import {IconProps} from 'cloud-providers/provider';
+import {IconProps} from '@kepler.gl/cloud-providers';
 
 const StyledUploader = styled.div`
   display: flex;
