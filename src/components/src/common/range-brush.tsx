8d7b404cf2fb6e7b2b31a4ca138ee52f12a84780
Jarrad Whitaker <jwhitaker-swiftnav@users.noreply.github.com>
2022-11-30 13:57:58 +1100

    [Bug] make sure the RangeBrush updates on slider range changes
    Before this commit, if you have, e.g., a Slider with
    
        range={[0,10]},
        value={[4,5]},
    
    , then it and its RangeBrush will look like
    
        ----||----- <- RangeBrush/histogram
        ====||===== <- Slider
    
    . A bug will then appear if you change the Slider's range, say
    to
    
        range={[0,5]},
        value={[4,5]},
    
    : then you will see a situation like
    
        ----||----- <- RangeBrush (**incorrect**)
        --------|-| <- Slider (correct)
    
    where the RangeBrush has failed to update and so looks out
    of sync with its Slider. This commit fixes the problem.
    
    Signed-off-by: Jarrad Whitaker <jwhitaker-swiftnav@users.noreply.github.com>

--- src/components/src/common/range-brush.tsx
+++ src/components/src/common/range-brush.tsx
@@ -172,8 +172,8 @@ function RangeBrushFactory(): React.ComponentType<RangeBrushProps> {
       } = this.props;
       const [prevVal0, prevVal1] = prevProps.value;
 
-      if (prevProps.width !== width) {
-        // width change should not trigger this._brushed
+      if (prevProps.width !== width || prevProps.range[0] !== this.props.range[0] || prevProps.range[1] !== this.props.range[1]) {
+        // dimension change should not trigger this._brushed
         this.moving = true;
         if (this.brush) this.root?.call(this.brush);
         this._move(val0, val1);
