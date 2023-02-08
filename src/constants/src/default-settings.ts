d763bd075c6109a4befe3c34dd0a665ea8099efe
Ihor Dykhta <dikhta.igor@gmail.com>
2023-02-08 12:55:25 +0200

    fix tests
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/constants/src/default-settings.ts
+++ src/constants/src/default-settings.ts
@@ -667,7 +667,9 @@ export const FIELD_OPTS = {
 export const CHANNEL_SCALE_SUPPORTED_FIELDS = Object.keys(CHANNEL_SCALES).reduce(
   (accu, key) => ({
     ...accu,
-    [key]: Object.keys(FIELD_OPTS).filter(ft => Object.keys(FIELD_OPTS[ft].scale[key]).length)
+    [key]: Object.keys(FIELD_OPTS).filter(
+      ft => FIELD_OPTS[ft].scale[key] && Object.keys(FIELD_OPTS[ft].scale[key]).length
+    )
   }),
   {} as {[id: string]: string[]}
 );
