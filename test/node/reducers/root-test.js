e1e165e69a71b5ce391a01438eff7111de332344
Igor D <igorDykhta@users.noreply.github.com>
2023-05-07 03:10:37 +0300

    [Feat] Added new options parameter to override single action reducer default behavior (#2217)
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>
    Co-authored-by: Giuseppe Macrì <macri.giuseppe@gmail.com>

--- test/node/reducers/root-test.js
+++ test/node/reducers/root-test.js
@@ -25,7 +25,10 @@ import {
   resetMapConfig,
   receiveMapConfig,
   toggleSplitMap,
-  toggleMapControl
+  toggleMapControl,
+  layerTypeChange,
+  addDataToMap,
+  ActionTypes
 } from '@kepler.gl/actions';
 import {createAction, handleActions} from 'redux-actions';
 
@@ -187,7 +190,7 @@ test('keplerGlReducer.plugin', t => {
   const testReducer = keplerGlReducer
     // 1. as reducer map
     .plugin({
-      HIDE_AND_SHOW_SIDE_PANEL: (state, action) => ({
+      HIDE_AND_SHOW_SIDE_PANEL: state => ({
         ...state,
         uiState: {
           ...state.uiState,
@@ -199,7 +202,7 @@ test('keplerGlReducer.plugin', t => {
       handleActions(
         {
           // 2. as reducer
-          HIDE_MAP_CONTROLS: (state, action) => ({
+          HIDE_MAP_CONTROLS: state => ({
             ...state,
             uiState: {
               ...state.uiState,
@@ -221,6 +224,92 @@ test('keplerGlReducer.plugin', t => {
   // dispatch action 2
   const updatedState2 = testReducer(testInitialState, hideMapControls());
   t.equal(updatedState2.test3.uiState.mapControls, hiddenMapControl, 'should call hideMapControls');
+
+  t.end();
+});
+
+test('keplerGlReducer.plugin override', t => {
+  // custom actions
+  const mockRawData = {
+    fields: [
+      {
+        name: 'start_point_lat',
+        id: 'start_point_lat',
+        displayName: 'start_point_lat',
+        type: 'real',
+        fieldIdx: 0
+      },
+      {
+        name: 'start_point_lng',
+        id: 'start_point_lng',
+        displayName: 'start_point_lng',
+        type: 'real',
+        fieldIdx: 2
+      },
+      {
+        name: 'end_point_lat',
+        id: 'end_point_lat',
+        displayName: 'end_point_lat',
+        type: 'real',
+        fieldIdx: 3
+      },
+      {
+        name: 'end_point_lng',
+        id: 'end_point_lng',
+        displayName: 'end_point_lng',
+        type: 'real',
+        fieldIdx: 4
+      }
+    ],
+    rows: [
+      [12.25, 37.75, 45.21, 100.12],
+      [null, 35.2, 45.0, 21.3],
+      [12.29, 37.64, 46.21, 99.127],
+      [null, null, 33.1, 29.34]
+    ]
+  };
+
+  const testReducer = keplerGlReducer
+    // 1. as reducer map
+    .plugin(
+      {
+        [ActionTypes.LAYER_TYPE_CHANGE]: (state, action) => {
+          return {
+            ...state,
+            visState: {
+              ...state.visState,
+              // do the default behavior and update layerOrder to empty
+              layerOrder: []
+            }
+          };
+        }
+      },
+      {override: {[ActionTypes.LAYER_TYPE_CHANGE]: true}}
+    );
+
+  let nextState = testReducer(undefined, registerEntry({id: 'test3'}));
+
+  nextState = testReducer(
+    nextState,
+    addDataToMap({
+      datasets: {
+        data: mockRawData,
+        info: {
+          id: 'foo'
+        }
+      }
+    })
+  );
+
+  t.equal(nextState.test3.visState.layers.length, 4, 'Should have 4 layer');
+
+  nextState = testReducer(nextState, layerTypeChange(nextState.test3.visState.layers[0], 'arc'));
+  t.equal(
+    nextState.test3.visState.layers[0].type,
+    'point',
+    'Should have not changed layer type to arc'
+  );
+  t.deepEqual(nextState.test3.visState.layerOrder, [], 'Should have changed layerOrder to empty');
 
   t.end();
 });
