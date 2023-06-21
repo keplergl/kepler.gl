46df6014868e27865bc52d1d6c4a78dc51fd5d48
Igor D <igorDykhta@users.noreply.github.com>
2023-06-21 17:52:34 +0300

    [Chore] improved saved layer and interaction type (#2275)
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>
    Co-authored-by: Shan He <heshan0131@gmail.com>

--- src/schemas/src/schema-manager.ts
+++ src/schemas/src/schema-manager.ts
@@ -40,9 +40,9 @@ export type SavedMapState = {
   pitch: number;
   zoom: number;
   isSplit: boolean;
-  isViewportSynced: true;
-  isZoomLocked: false;
-  splitMapViewports: [];
+  isViewportSynced?: true;
+  isZoomLocked?: false;
+  splitMapViewports?: [];
 };
 
 export type SavedLayerGroups = {
