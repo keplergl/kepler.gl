9bf58a1144097c1642900fe7da4178821a58eee2
Ihor Dykhta <dikhta.igor@gmail.com>
2023-10-22 18:46:13 +0300

    [chore] fix lint in cmpEffects
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- test/helpers/comparison-utils.js
+++ test/helpers/comparison-utils.js
@@ -201,11 +201,11 @@ export function cmpSavedLayers(t, expectedLayer, actualLayer, opt = {}, idx = ''
   }
 }
 
-export function cmpEffects(t, expectedEffect, actualEffect, opts = {}) {
+export function cmpEffects(t, expectedEffect, actualEffect, opts = {}, idx = '') {
   if (Array.isArray(expectedEffect) && Array.isArray(actualEffect)) {
     t.equal(actualEffect.length, expectedEffect.length, 'should have same number of effects');
     expectedEffect.forEach((_, i) => {
-      cmpEffects(t, expectedEffect[i], actualEffect[i], opt, String(i));
+      cmpEffects(t, expectedEffect[i], actualEffect[i], opts, String(i));
     });
   } else {
     if (!expectedEffect || !actualEffect) {
