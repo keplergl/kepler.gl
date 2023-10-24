b54c1739fb2a39cc329bf0def1df9c08f9aa1f12
Ib Green <ib@unfolded.ai>
2023-10-24 12:35:05 -0400

    chore: Upgrade to loaders.gl@4.0 (#2394)
    Signed-off-by: Ib Green <ib@foursquare.com>

--- babel-register.js
+++ babel-register.js
@@ -21,6 +21,7 @@
 const forceTranspile = [
   // ESM libraries that require transpilation
   /@deck.gl\/layers/,
+  /@loaders.gl\/polyfills/,
   // For some reason babel crashes even before trying to transpile this library
   // Instead we force transpile @deck.gl/layers which includes it, and alias to a transpiled version in babel.config.js
   /@mapbox\/tiny-sdf/
