676ffd6065a46a04b519a36b2fe3a3d30eca8bdc
Giuseppe Macri <gmacri@foursquare.com>
2023-11-17 14:09:22 -0500

    only provider def
    Signed-off-by: Giuseppe Macri <gmacri@foursquare.com>

--- examples/demo-app/src/cloud-providers/foursquare/foursquare-provider.js
+++ examples/demo-app/src/cloud-providers/foursquare/foursquare-provider.js
@@ -19,9 +19,8 @@
 // THE SOFTWARE.
 
 import FSQIcon from './foursquare-icon';
-import {Provider} from 'kepler.gl/cloud-providers';
+import {Provider, KEPLER_FORMAT} from 'kepler.gl/cloud-providers';
 import {Auth0Client} from '@auth0/auth0-spa-js';
-import {KEPLER_FORMAT} from '@kepler.gl/cloud-providers';
 
 const NAME = 'Foursquare';
 const DISPLAY_NAME = 'Foursquare';
@@ -60,133 +59,133 @@ function extractMapFromFSQResponse(response) {
 }
 
 export default class FoursquareProvider extends Provider {
-  constructor({clientId, authDomain, apiURL, userMapsURL}) {
-    super({name: NAME, displayName: DISPLAY_NAME, icon: FSQIcon});
-    this.icon = FSQIcon;
-    this.appName = APP_NAME;
-    this.apiURL = apiURL;
-
-    const redirect_uri = window.location.origin + window.location.pathname;
-
-    this._auth0 = new Auth0Client({
-      domain: authDomain,
-      clientId: clientId,
-      scope: FOURSQUARE_AUTH_SCOPE,
-      authorizationParams: {
-        redirect_uri,
-        audience: FOURSQUARE_AUTH_AUDIENCE
-      },
-      cacheLocation: 'localstorage'
-    });
-
-    // the domain needs to be passed as input param
-    this._folderLink = userMapsURL;
-    this.isNew = true;
-  }
-
-  hasPrivateStorage() {
-    return FOURSQUARE_PRIVATE_STORAGE_ENABLED;
-  }
-
-  async getUser() {
-    return this._auth0.getUser();
-  }
-
-  async login() {
-    return this._auth0.loginWithPopup();
-  }
-
-  async logout() {
-    return this._auth0.logout({
-      // this make sure after logging out the sdk will not redirect the user
-      openUrl: false
-    });
-  }
-
-  async uploadMap({mapData, options = {}}) {
-    // TODO: handle replace
-    // const mode = options.overwrite ? 'overwrite' : 'add';
-    const {map, thumbnail} = mapData;
-
-    const {title = '', description = ''} = map.info;
-    const headers = await this.getHeaders();
-    const payload = {
-      name: title,
-      description,
-      importSource: FOURSQUARE_KEPLER_GL_IMPORT_SOURCE,
-      latestState: {
-        data: map
-      }
-    };
-
-    const mapResponse = await fetch(`${this.apiURL}/v1/maps`, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(payload)
-    });
-
-    const createMap = await mapResponse.json();
-
-    await fetch(`${this.apiURL}/v1/maps/${createMap.id}/thumbnail`, {
-      method: 'PUT',
-      headers: {
-        ...headers,
-        'Content-Type': 'image/png'
-      },
-      body: thumbnail
-    });
-
-    return map;
-  }
-
-  async listMaps() {
-    const headers = await this.getHeaders();
-    const response = await fetch(
-      `${this.apiURL}/v1/maps?importSource=${FOURSQUARE_KEPLER_GL_IMPORT_SOURCE}`,
-      {
-        method: 'GET',
-        mode: 'cors',
-        headers
-      }
-    );
-    const data = await response.json();
-    return data.items.map(convertFSQModelToMapItem);
-  }
-
-  async downloadMap(loadParams) {
-    const {mapId} = loadParams;
-    if (!mapId) {
-      return Promise.reject('No Map is was provider as part of loadParams');
-    }
-    const headers = await this.getHeaders();
-
-    const response = await fetch(`${this.apiURL}/v1/maps/${mapId}`, {
-      method: 'GET',
-      headers
-    });
-
-    const map = await response.json();
-
-    return Promise.resolve({
-      map: extractMapFromFSQResponse(map),
-      format: KEPLER_FORMAT
-    });
-  }
-
-  getManagementUrl() {
-    return this._folderLink;
-  }
-
-  async getAccessToken() {
-    return this._auth0.getTokenSilently();
-  }
-
-  async getHeaders() {
-    const accessToken = await this.getAccessToken();
-    return {
-      Authorization: `Bearer ${accessToken}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    };
-  }
+  // constructor({clientId, authDomain, apiURL, userMapsURL}) {
+  //   super({name: NAME, displayName: DISPLAY_NAME, icon: FSQIcon});
+  //   this.icon = FSQIcon;
+  //   this.appName = APP_NAME;
+  //   this.apiURL = apiURL;
+  //
+  //   const redirect_uri = window.location.origin + window.location.pathname;
+  //
+  //   this._auth0 = new Auth0Client({
+  //     domain: authDomain,
+  //     clientId: clientId,
+  //     scope: FOURSQUARE_AUTH_SCOPE,
+  //     authorizationParams: {
+  //       redirect_uri,
+  //       audience: FOURSQUARE_AUTH_AUDIENCE
+  //     },
+  //     cacheLocation: 'localstorage'
+  //   });
+  //
+  //   // the domain needs to be passed as input param
+  //   this._folderLink = userMapsURL;
+  //   this.isNew = true;
+  // }
+  //
+  // hasPrivateStorage() {
+  //   return FOURSQUARE_PRIVATE_STORAGE_ENABLED;
+  // }
+  //
+  // async getUser() {
+  //   return this._auth0.getUser();
+  // }
+  //
+  // async login() {
+  //   return this._auth0.loginWithPopup();
+  // }
+  //
+  // async logout() {
+  //   return this._auth0.logout({
+  //     // this make sure after logging out the sdk will not redirect the user
+  //     openUrl: false
+  //   });
+  // }
+  //
+  // async uploadMap({mapData, options = {}}) {
+  //   // TODO: handle replace
+  //   // const mode = options.overwrite ? 'overwrite' : 'add';
+  //   const {map, thumbnail} = mapData;
+  //
+  //   const {title = '', description = ''} = map.info;
+  //   const headers = await this.getHeaders();
+  //   const payload = {
+  //     name: title,
+  //     description,
+  //     importSource: FOURSQUARE_KEPLER_GL_IMPORT_SOURCE,
+  //     latestState: {
+  //       data: map
+  //     }
+  //   };
+  //
+  //   const mapResponse = await fetch(`${this.apiURL}/v1/maps`, {
+  //     method: 'POST',
+  //     headers,
+  //     body: JSON.stringify(payload)
+  //   });
+  //
+  //   const createMap = await mapResponse.json();
+  //
+  //   await fetch(`${this.apiURL}/v1/maps/${createMap.id}/thumbnail`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       ...headers,
+  //       'Content-Type': 'image/png'
+  //     },
+  //     body: thumbnail
+  //   });
+  //
+  //   return map;
+  // }
+  //
+  // async listMaps() {
+  //   const headers = await this.getHeaders();
+  //   const response = await fetch(
+  //     `${this.apiURL}/v1/maps?importSource=${FOURSQUARE_KEPLER_GL_IMPORT_SOURCE}`,
+  //     {
+  //       method: 'GET',
+  //       mode: 'cors',
+  //       headers
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   return data.items.map(convertFSQModelToMapItem);
+  // }
+  //
+  // async downloadMap(loadParams) {
+  //   const {mapId} = loadParams;
+  //   if (!mapId) {
+  //     return Promise.reject('No Map is was provider as part of loadParams');
+  //   }
+  //   const headers = await this.getHeaders();
+  //
+  //   const response = await fetch(`${this.apiURL}/v1/maps/${mapId}`, {
+  //     method: 'GET',
+  //     headers
+  //   });
+  //
+  //   const map = await response.json();
+  //
+  //   return Promise.resolve({
+  //     map: extractMapFromFSQResponse(map),
+  //     format: KEPLER_FORMAT
+  //   });
+  // }
+  //
+  // getManagementUrl() {
+  //   return this._folderLink;
+  // }
+  //
+  // async getAccessToken() {
+  //   return this._auth0.getTokenSilently();
+  // }
+  //
+  // async getHeaders() {
+  //   const accessToken = await this.getAccessToken();
+  //   return {
+  //     Authorization: `Bearer ${accessToken}`,
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json'
+  //   };
+  // }
 }
