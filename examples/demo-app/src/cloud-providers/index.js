6859d0e9009997cc36f4479fbc98da4cba2573da
Giuseppe Macri <gmacri@foursquare.com>
2023-11-17 12:29:34 -0500

    added provider
    Signed-off-by: Giuseppe Macri <gmacri@foursquare.com>

--- examples/demo-app/src/cloud-providers/index.js
+++ examples/demo-app/src/cloud-providers/index.js
@@ -22,6 +22,7 @@ import {CLOUD_PROVIDERS_CONFIGURATION} from '../constants/default-settings';
 
 import DropboxProvider from './dropbox/dropbox-provider';
 import CartoProvider from './carto/carto-provider';
+import FoursquareProvider from './foursquare/foursquare-provider';
 
 const {
   DROPBOX_CLIENT_ID,
@@ -44,6 +45,12 @@ const DROPBOX_CLIENT_NAME = 'Kepler.gl Demo App';
 export const DEFAULT_CLOUD_PROVIDER = 'dropbox';
 
 export const CLOUD_PROVIDERS = [
+  new FoursquareProvider({
+    clientId: FOURSQUARE_CLIENT_ID,
+    authDomain: FOURSQUARE_DOMAIN,
+    apiURL: FOURSQUARE_API_URL,
+    userMapsURL: FOURSQUARE_USER_MAPS_URL
+  }),
   new DropboxProvider(DROPBOX_CLIENT_ID, DROPBOX_CLIENT_NAME),
   new CartoProvider(CARTO_CLIENT_ID)
 ];
