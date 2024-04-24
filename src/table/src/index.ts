940723be10617168e32ea86ed1bd4e8741a214fb
Igor Dykhta <igorDykhta@users.noreply.github.com>
2024-04-25 02:17:43 +0300

    ts fixes
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/table/src/index.ts
+++ src/table/src/index.ts
@@ -12,6 +12,6 @@ export {
   maybeToDate
 } from './kepler-table';
 /* eslint-disable prettier/prettier */
-export type {GpuFilter, Datasets} from './kepler-table';
+export type {FilterProps, GpuFilter, Datasets, BooleanFieldFilterProps} from './kepler-table';
 export * from './gpu-filter-utils';
 export * from './dataset-utils';
