133b58e93dee1d22fc8b4b391f747aa3807f76e1
Alex Bain <alex_s_bain@apple.com>
2024-07-25 11:07:57 -0700

    Adds a @ts-ignore to resolve a build failure related to an unknown type
    Signed-off-by: Alex Bain <alex_s_bain@apple.com>

--- src/processors/src/data-processor.ts
+++ src/processors/src/data-processor.ts
@@ -383,6 +383,7 @@ export function processKeplerglDataset(
  * @returns dataset containing `fields` and `rows` or null
  */
 export function processArrowTable(arrowTable: ArrowTable): ProcessorResult | null {
+  // @ts-ignore - Unknown data type causing build failures
   return processArrowBatches(arrowTable.data.batches);
 }
 
