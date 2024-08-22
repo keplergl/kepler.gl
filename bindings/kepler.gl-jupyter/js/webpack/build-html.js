f6a696b7fef6e949e38aed834325b01832013b41
jameswillis <james@wherobots.com>
2024-08-21 21:51:40 -0700

    fix version of reactIntl to one that has a UMD file

--- bindings/kepler.gl-jupyter/js/webpack/build-html.js
+++ bindings/kepler.gl-jupyter/js/webpack/build-html.js
@@ -20,7 +20,7 @@ const VERSIONS = {
   reactDom:clearCarats(dependencies['react-dom']),
   redux: clearCarats(dependencies.redux),
   reactRedux: clearCarats(dependencies['react-redux']),
-  reactIntl: clearCarats(dependencies['react-intl']),
+  reactIntl: '4.7.6',
   reactCopyToClipboard: clearCarats(dependencies['react-copy-to-clipboard']),
   styledComponents: clearCarats(dependencies['styled-components']),
   keplergl: clearCarats(dependencies['kepler.gl'])
