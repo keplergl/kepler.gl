0825a8eab509893b19a0890015b6ccf46f8d2ffa
Ihor Dykhta <dikhta.igor@gmail.com>
2024-09-09 22:57:41 +0300

    [fix] Improved map bounds calculation and handled latitude issues
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/reducers/src/data-utils.ts
+++ src/reducers/src/data-utils.ts
@@ -10,6 +10,34 @@ const MAX_LONGITUDE = 180;
 const MIN_LONGITUDE = -180;
 
 /**
+ * takes a list of layer bounds and returns a single bound
+ */
+export function processLayerBounds(layerBounds: Bounds[]): Bounds {
+  return layerBounds.reduce(
+    (res, b) => {
+      const minLongitude = Math.min(res[0], b[0]);
+      const minLatitude = Math.min(res[1], b[1]);
+      const maxLongitude = Math.max(res[2], b[2]);
+      const maxLatitude = Math.max(res[3], b[3]);
+
+      // for some reason WebMercatorViewport can't handle latitude -90,90 and throws an error
+      // so we default to lat/lng (0,0)
+      // viewport.js:81 Uncaught Error: Pixel project matrix not invertible
+      // at WebMercatorViewport16.Viewport5 (viewport.js:81:13)
+      // at new WebMercatorViewport16 (web-mercator-viewport.js:92:5)
+      // at getViewportFromMapState (map-utils.js:46:66)
+      return [
+        minLongitude <= MIN_LONGITUDE ? 0 : minLongitude,
+        minLatitude <= MIN_LATITUDE ? 0 : minLatitude,
+        maxLongitude >= MAX_LONGITUDE ? 0 : maxLongitude,
+        maxLatitude >= MAX_LATITUDE ? 0 : maxLatitude
+      ];
+    },
+    [MAX_LONGITUDE, MAX_LATITUDE, MIN_LONGITUDE, MIN_LATITUDE]
+  );
+}
+
+/**
  * return center of map from given points
  * @param layers
  * @returns coordinates of map center, empty if not found
@@ -29,17 +57,5 @@ export function findMapBounds(layers: Layer[]): Bounds | null {
     return null;
   }
   // merge bounds in each layer
-  const newBounds = (availableLayerBounds as Bounds).reduce(
-    (res, b) => {
-      return [
-        Math.min(res[0], b[0]),
-        Math.min(res[1], b[1]),
-        Math.max(res[2], b[2]),
-        Math.max(res[3], b[3])
-      ];
-    },
-    [MAX_LONGITUDE, MAX_LATITUDE, MIN_LONGITUDE, MIN_LATITUDE]
-  );
-  // @ts-expect-error
-  return newBounds;
+  return processLayerBounds(availableLayerBounds);
 }
