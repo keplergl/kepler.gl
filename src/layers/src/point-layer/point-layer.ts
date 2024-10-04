21a7611dd4168f6219646ebc4cb15024df221e70
Ihor Dykhta <dikhta.igor@gmail.com>
2024-10-04 18:14:35 +0300

    remove checks for arrow container
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/layers/src/point-layer/point-layer.ts
+++ src/layers/src/point-layer/point-layer.ts
@@ -627,7 +627,7 @@ export default class PointLayer extends Layer {
 
   hasHoveredObject(objectInfo) {
     // non-GeoArrow mode
-    if (!(this.dataContainer instanceof ArrowDataContainer)) {
+    if (this.config.columnMode !== COLUMN_MODE_GEOARROW) {
       return super.hasHoveredObject(objectInfo);
     }
 
@@ -653,7 +653,7 @@ export default class PointLayer extends Layer {
     // for arrow format, `object` is the Arrow row object Proxy,
     // and index is passed in `hoverInfo`.
     const index =
-      dataContainer instanceof ArrowDataContainer
+      this.config.columnMode === COLUMN_MODE_GEOARROW
         ? hoverInfo?.index
         : (object as {index: number}).index;
     if (index >= 0) {
