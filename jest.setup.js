7220ae429555dbe697991accb13fbf0af50c75d2
Ihor Dykhta <dikhta.igor@gmail.com>
2024-10-23 22:30:33 +0300

    fix jest
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- jest.setup.js
+++ jest.setup.js
@@ -3,12 +3,16 @@
 
 import '@testing-library/jest-dom';
 
-// ReferenceError: ReadableStream is not defined in @loaders.gl/polyfills
-import {ReadableStream} from 'node:stream/web';
-this.global.ReadableStream = ReadableStream;
+// Disable polyfils as // ReferenceError: ReadableStream is not defined in @loaders.gl/polyfills
+// import {installFilePolyfills} from '@loaders.gl/polyfills';
+// installFilePolyfills();
 
-import {installFilePolyfills} from '@loaders.gl/polyfills';
-installFilePolyfills();
+// Remove once @loaders.gl/polyfills are reenabled
+const {TextDecoder, TextEncoder} = require('node:util');
+Object.defineProperties(globalThis, {
+  TextDecoder: {value: TextDecoder},
+  TextEncoder: {value: TextEncoder}
+});
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
   Map: () => ({})
@@ -19,5 +23,8 @@ jest.mock('@kepler.gl/utils', () => ({
   hasPortableWidth: jest.fn(),
   hasMobileWidth: jest.fn()
 }));
+
+// @loaders.gl/parquet isn't tested in jest atm, and is generating errors
+jest.mock('@loaders.gl/parquet', () => ({}));
 
 global.URL.createObjectURL = jest.fn();
