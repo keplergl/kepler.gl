f876e09ea029543a69600b4a4e14bb2fe384e9ab
Xun Li <lixun910@gmail.com>
2024-11-18 19:15:00 -0700

    test Netlify build

--- src/ai-assistant/src/index.ts
+++ src/ai-assistant/src/index.ts
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright contributors to the kepler.gl project
 
-export * from './localization';
-
 export {default as AiAssistantManagerFactory} from './components/ai-assistant-manager';
 export {default as AiAssistantConfigFactory} from './components/ai-assistant-config';
 export {default as AiAssistantComponentFactory} from './components/ai-assistant-component';
@@ -10,6 +8,6 @@ export {default as AiAssistantControlFactory} from './map/ai-assistant-control';
 
 export type {AiAssistantState, AiAssistantConfig} from './reducers';
 
-export * from './reducers';
-export * from './actions';
+export {aiAssistantReducer} from './reducers';
+export {setStartScreenCapture, setScreenCaptured} from './actions';
 export * from './localization';
