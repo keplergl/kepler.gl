e2278aada836ed714ae796e8a5f155ff22ddeb34
Ihor Dykhta <dikhta.igor@gmail.com>
2024-12-07 22:36:57 +0200

    lint fixes
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/common-utils/src/h3-utils.ts
+++ src/common-utils/src/h3-utils.ts
@@ -3,7 +3,6 @@
 
 import {h3GetResolution, H3Index, h3IsValid, h3ToGeo, h3ToGeoBoundary} from 'h3-js';
 import {ALL_FIELD_TYPES} from '@kepler.gl/constants';
-import {notNullorUndefined} from '@kepler.gl/common-utils';
 
 export {h3GetResolution, h3IsValid};
 
