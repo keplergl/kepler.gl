2d1d8e5f51bfad62879e408195908d0e6cb35dbd
Xun Li <lixun910@gmail.com>
2024-12-27 10:23:41 -0700

    [Fix] add_data() function in keplergl-jupyter (#2867)
    Publish Keplergl-jupyter 0.3.5

--- bindings/kepler.gl-jupyter/keplergl/keplergl.py
+++ bindings/kepler.gl-jupyter/keplergl/keplergl.py
@@ -221,14 +221,14 @@ class KeplerGl(widgets.DOMWidget):
         if use_arrow:
             global g_use_arrow
             g_use_arrow = use_arrow
-            try:
-                gdf = geopandas.read_file(data, driver='GeoJSON')
-                copy.update({name: gdf})
-            except Exception:
-                # if it fails, assume it is a csv string
-                # load csv string to a dataframe
-                df = pd.read_csv(data)
-                copy.update({name: df})
+        try:
+            gdf = geopandas.read_file(data, driver='GeoJSON')
+            copy.update({name: gdf})
+        except Exception:
+            # if it fails, assume it is a csv string
+            # load csv string to a dataframe
+            df = pd.read_csv(data)
+            copy.update({name: df})
 
         self.data = copy
 
