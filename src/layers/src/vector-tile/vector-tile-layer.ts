f136bb511608bbc2f7ad451843e1e174452e348f
Ihor Dykhta <dikhta.igor@gmail.com>
2025-01-08 23:48:46 +0200

    fix fill highlighting
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/layers/src/vector-tile/vector-tile-layer.ts
+++ src/layers/src/vector-tile/vector-tile-layer.ts
@@ -70,7 +70,8 @@ import {
   getPropertyByZoom
 } from './common-tile/tile-utils';
 
-export const DEFAULT_HIGHLIGHT_COLOR = [252, 242, 26, 150];
+export const DEFAULT_HIGHLIGHT_FILL_COLOR = [252, 242, 26, 150];
+export const DEFAULT_HIGHLIGHT_STROKE_COLOR = [252, 242, 26, 255];
 export const MAX_CACHE_SIZE_MOBILE = 1; // Minimize caching, visible tiles will always be loaded
 export const DEFAULT_STROKE_WIDTH = 1;
 
@@ -537,7 +538,7 @@ export default class VectorTileLayer extends AbstractTileLayer<VectorTile, Featu
 
           pointRadiusMinPixels: 1,
           autoHighlight: true,
-          highlightColor: DEFAULT_HIGHLIGHT_COLOR,
+          highlightColor: DEFAULT_HIGHLIGHT_FILL_COLOR,
           pickable: true,
           transitions,
           updateTriggers: {
@@ -611,17 +612,18 @@ export default class VectorTileLayer extends AbstractTileLayer<VectorTile, Featu
         ...(hoveredObject
           ? [
               new GeoJsonLayer({
-                // @ts-expect-error not typed
+                // @ts-expect-error props not typed?
                 ...objectHovered.sourceLayer?.props,
                 ...(this.getDefaultHoverLayerProps() as any),
                 visible: true,
                 wrapLongitude: false,
                 data: [hoveredObject],
-                getLineColor: this.config.highlightColor,
-                getFillColor: this.config.highlightColor,
-                // always draw outline
+                getLineColor: DEFAULT_HIGHLIGHT_STROKE_COLOR,
+                getFillColor: DEFAULT_HIGHLIGHT_FILL_COLOR,
+                getLineWidth: visConfig.strokeWidth + 1,
+                lineWidthUnits: 'pixels',
                 stroked: true,
-                filled: false
+                filled: true
               })
             ]
           : [])
