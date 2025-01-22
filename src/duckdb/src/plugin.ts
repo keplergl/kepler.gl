07ad748711be5a3c7e3031d37f2f9307e6828ec4
Ihor Dykhta <dikhta.igor@gmail.com>
2025-01-22 19:44:55 +0200

    update licence
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/duckdb/src/plugin.ts
+++ src/duckdb/src/plugin.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the kepler.gl project
+
 import {initializeDuckDb} from './init';
 
 export const keplerGlDuckDBPlugin = {
--- src/duckdb/src/processors/data-processor.ts
+++ src/duckdb/src/processors/data-processor.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the kepler.gl project
+
 import * as arrow from 'apache-arrow';
 import normalize from '@mapbox/geojson-normalize';
 
