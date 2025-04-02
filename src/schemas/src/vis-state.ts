fdd698ae7d8fa2f8bb87a8c02cd34070cc2d3fc6
Shan He <heshan0131@gmail.com>
2025-04-02 15:06:02 -0700

    add more vis-state schema
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- src/schemas/src/vis-state.ts
+++ src/schemas/src/vis-state.ts
@@ -2,22 +2,33 @@
 
 import {z} from 'zod';
 
-import {AnimationConfigSchema} from './animation';
-import {EffectSchema} from './effect';
-import {FilterSchema} from './filter';
+import {AnimationConfigSchema} from './vis-state/animation';
+import {EffectSchema} from './vis-state/effect';
+import {FilterSchema} from './vis-state/filter';
 import {LayerSchema} from './layers';
-import {InteractionConfigSchema} from './interaction-config';
+import {InteractionConfigSchema} from './vis-state/interaction-config';
+import {EditorSchema} from './vis-state/editor';
 
 export const VisStateSchema = z.object({
   effects: z.array(EffectSchema).optional().describe('Effects to apply to the map layers'),
   filters: z.array(FilterSchema).optional().describe('Filters to apply to the data'),
   layers: z.array(LayerSchema).default([]).describe('Layers to display in the map'),
-  interactionConfig: z.unknown(),
-  layerBlending: z.unknown(),
-  overlayBlending: z.unknown().optional(),
+  interactionConfig: InteractionConfigSchema.optional().describe(
+    'Interaction configuration for the map, setting tooltips, geocoder, etc.'
+  ),
+  layerBlending: z
+    .enum(['additive', 'normal', 'subtractive'])
+    .default('normal')
+    .optional()
+    .describe('How layers are blended together on top of the map'),
+  overlayBlending: z
+    .enum(['normal', 'screen', 'darken'])
+    .default('normal')
+    .optional()
+    .describe('How map overlayers are blended with the map'),
   splitMaps: z.unknown(),
   animationConfig: AnimationConfigSchema.optional().describe('Configuration for animating the map'),
-  editor: z.unknown().optional(),
+  editor: EditorSchema.optional(),
   datasets: z.unknown(),
   layerOrder: z.unknown()
 });
