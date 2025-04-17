e935ef51dc3201a22a79bf3b049dbef23d9f891a
Igor Dykhta <igorDykhta@users.noreply.github.com>
2025-04-18 02:09:02 +0300

    [chore] extra exports from duckdb module (#3050)
    * [chore] extra exports from duckdb moduel
    
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>
    
    * export all duck utils
    
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>
    
    ---------
    
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/duckdb/src/index.ts
+++ src/duckdb/src/index.ts
@@ -5,10 +5,5 @@ export {DuckDBWasmAdapter} from './adapters/duckdb-wasm-adapter';
 export {SqlPanel} from './components';
 import {keplerGlDuckDBPlugin} from './plugin';
 export {KeplerGlDuckDbTable} from './table/duckdb-table';
-export {
-  getDuckDBColumnTypes,
-  getDuckDBColumnTypesMap,
-  getGeometryColumns,
-  constructST_asWKBQuery
-} from './table/duckdb-table-utils';
+export * from './table/duckdb-table-utils';
 export default keplerGlDuckDBPlugin;
