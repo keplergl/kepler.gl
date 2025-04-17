9011eef7cd5d8a268fcaf7630e0dc3e96cc32a14
Ihor Dykhta <dikhta.igor@gmail.com>
2025-04-18 01:56:32 +0300

    export all duck utils
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/duckdb/src/index.ts
+++ src/duckdb/src/index.ts
@@ -5,11 +5,5 @@ export {DuckDBWasmAdapter} from './adapters/duckdb-wasm-adapter';
 export {SqlPanel} from './components';
 import {keplerGlDuckDBPlugin} from './plugin';
 export {KeplerGlDuckDbTable} from './table/duckdb-table';
-export {
-  constructST_asWKBQuery,
-  getDuckDBColumnTypes,
-  getDuckDBColumnTypesMap,
-  getGeometryColumns,
-  setGeoArrowWKBExtension
-} from './table/duckdb-table-utils';
+export * from './table/duckdb-table-utils';
 export default keplerGlDuckDBPlugin;
