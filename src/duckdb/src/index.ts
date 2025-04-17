e5c2b831c0da35d760c449ba7609582fb14a4000
Ihor Dykhta <dikhta.igor@gmail.com>
2025-04-18 01:41:43 +0300

    [chore] extra exports from duckdb moduel
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/duckdb/src/index.ts
+++ src/duckdb/src/index.ts
@@ -6,9 +6,10 @@ export {SqlPanel} from './components';
 import {keplerGlDuckDBPlugin} from './plugin';
 export {KeplerGlDuckDbTable} from './table/duckdb-table';
 export {
+  constructST_asWKBQuery,
   getDuckDBColumnTypes,
   getDuckDBColumnTypesMap,
   getGeometryColumns,
-  constructST_asWKBQuery
+  setGeoArrowWKBExtension
 } from './table/duckdb-table-utils';
 export default keplerGlDuckDBPlugin;
