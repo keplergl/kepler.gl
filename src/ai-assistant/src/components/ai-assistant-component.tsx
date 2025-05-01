a8ebadb3b939e5d639a69f2c076718b8c264b65c
Xun Li <lixun910@gmail.com>
2025-05-01 14:06:22 -0700

    rebase; update import

--- src/ai-assistant/src/components/ai-assistant-component.tsx
+++ src/ai-assistant/src/components/ai-assistant-component.tsx
@@ -7,10 +7,8 @@ import styled from 'styled-components';
 import {textColorLT, theme} from '@kepler.gl/styles';
 import {MessageModel} from '@openassistant/core';
 import {AiAssistant} from '@openassistant/ui';
-import '@openassistant/echarts/../dist/index.esm.css';
-import '@openassistant/ui/../dist/bundle.css';
-// import '@openassistant/echarts/dist/index.css';
-// import '@openassistant/ui/dist/index.css';
+import '@openassistant/echarts/dist/index.css';
+import '@openassistant/ui/dist/index.css';
 
 import {setScreenCaptured, setStartScreenCapture, updateAiAssistantMessages} from '../actions';
 import {
