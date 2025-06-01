6c4ccd413088a1dac2eac9493c2eadd10194a471
Ihor Dykhta <dikhta.igor@gmail.com>
2025-06-01 23:10:39 +0300

    fix for highlighted object - hexbin layer
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/layers/src/hexagon-layer/hexagon-layer.ts
+++ src/layers/src/hexagon-layer/hexagon-layer.ts
@@ -135,7 +135,7 @@ export default class HexagonLayer extends AggregationLayer {
                 hexagonToPolygonGeo(hoveredObject, {}, radius * visConfig.coverage, mapState)
               ].filter(d => d),
               getLineColor: this.config.highlightColor,
-              lineWidthScale: clamp([1, 100], radius * 0.1 * zoomFactor)
+              lineWidthScale: 8 * zoomFactor
             })
           ]
         : [])
