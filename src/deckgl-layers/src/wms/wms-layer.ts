1c025f1fc908e44ea551f387176f85ec0e29911c
Ihor Dykhta <dikhta.igor@gmail.com>
2025-07-04 04:13:30 +0300

    fix broken import
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/deckgl-layers/src/wms/wms-layer.ts
+++ src/deckgl-layers/src/wms/wms-layer.ts
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright contributors to the kepler.gl project
 
+import {lngLatToWorld} from '@math.gl/web-mercator';
+
 import {
   Layer,
   CompositeLayer,
@@ -11,7 +13,6 @@ import {
   COORDINATE_SYSTEM,
   _deepEqual as deepEqual
 } from '@deck.gl/core/typed';
-import {WGS84ToPseudoMercator} from '@deck.gl/geo-layers/src/wms-layer/utils';
 import {BitmapLayer} from '@deck.gl/layers/typed';
 import {ImageSource, createImageSource} from '@loaders.gl/wms';
 
@@ -262,4 +263,18 @@ export default class WMSLayer extends CompositeLayer<Required<_WMSLayerProps>> {
     clearTimeout(this.state._timeoutId);
     this.state._timeoutId = setTimeout(() => fn(), ms);
   }
+}
+
+// https://epsg.io/3857
+// +proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs
+const HALF_EARTH_CIRCUMFERENCE = 6378137 * Math.PI;
+
+/** Projects EPSG:4326 to EPSG:3857
+ * This is a lightweight replacement of proj4. Use tests to ensure conformance.
+ */
+export function WGS84ToPseudoMercator(coord: [number, number]): [number, number] {
+  const mercator = lngLatToWorld(coord);
+  mercator[0] = (mercator[0] / 256 - 1) * HALF_EARTH_CIRCUMFERENCE;
+  mercator[1] = (mercator[1] / 256 - 1) * HALF_EARTH_CIRCUMFERENCE;
+  return mercator;
 }
