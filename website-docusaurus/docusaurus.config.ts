591478dc946bf061ffacf20eb5f71045fb575599
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-19 14:56:05 +0530

    Add katex styles
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- website-docusaurus/docusaurus.config.ts
+++ website-docusaurus/docusaurus.config.ts
@@ -157,7 +157,15 @@ const config: Config = {
       theme: prismThemes.oneLight,
       darkTheme: prismThemes.oneDark
     }
-  }
+  },
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+',
+      crossorigin: 'anonymous'
+    }
+  ]
 };
 
 export default config;
