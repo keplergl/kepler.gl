cc112266bea24e1e636e0792fdc9ef3ffa8f8ff8
Indranil Halder <indranilhalder.dev@gmail.com>
2025-09-01 15:13:35 +0530

    Add title and description
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

---
+++ website-secondary/src/components/layout/hero/hero.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react';
+
+const HeroSection: FC = () => {
+  return <div></div>;
+};
+
+export default HeroSection;
--- website-secondary/src/pages/index.tsx
+++ website-secondary/src/pages/index.tsx
@@ -1,44 +1,20 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+import type {FC, ReactNode} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import HeroSection from '../components/layout/hero/hero';
 
-function HomepageHeader() {
+const Home: FC = (): ReactNode => {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title={`${siteConfig.title}`}
+      description="Kepler.gl is a data agnostic, WebGL empowered, high-performance web application for geospatial analytic visualizations."
+    >
+      <HeroSection />
     </Layout>
   );
-}
+};
+
+export default Home;
