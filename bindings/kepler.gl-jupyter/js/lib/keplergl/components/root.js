c99de1fcb36f8e243e256a3a889cbef35f50a874
ckaros <ckaros@outlook.com>
2025-10-12 15:35:26 -0400

    wait for map to render before loading data and config
    Signed-off-by: karosc <ckaros@outlook.com>

--- bindings/kepler.gl-jupyter/js/lib/keplergl/components/root.js
+++ bindings/kepler.gl-jupyter/js/lib/keplergl/components/root.js
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: MIT
 // Copyright contributors to the kepler.gl project
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './app';
 
-function renderRoot({id, store, ele}) {
-  const Root = () => (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+function renderRoot({id, store, ele, onMounted}) {
+  const Root = () => {
+    useEffect(() => {
+      // Call onMounted callback after component is mounted
+      if (onMounted && typeof onMounted === 'function') {
+        onMounted();
+      }
+    }, []);
 
-  ReactDOM.render(<Root />, ele);
-}
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  };
+
+    ReactDOM.render(<Root />, ele);
+  }
 
 export default renderRoot;
