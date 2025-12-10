422c1b34756c3960fef3a07b4377bf9d10de1dea
Ilya Boyandin <ilya@boyandin.me>
2025-12-10 13:46:12 +0100

    fix: Avoid Monaco AMD bundle when importing `@kepler.gl/duckdb` (#3255)
    * chore: Split duckdb module export endpoints
    
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>
    
    * fixing stub script paths for the example app to run
    
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>
    
    * Addressed Copilot suggestion
    
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>
    
    ---------
    
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- src/duckdb/src/index.ts
+++ src/duckdb/src/index.ts
@@ -2,13 +2,7 @@
 // Copyright contributors to the kepler.gl project
 
 export {DuckDBWasmAdapter} from './adapters/duckdb-wasm-adapter';
-export {SqlPanel} from './components';
-import {keplerGlDuckDBPlugin} from './plugin';
-export {restoreGeoarrowMetadata, KeplerGlDuckDbTable} from './table/duckdb-table';
+export {keplerGlDuckDBPlugin, keplerGlDuckDBPlugin as default} from './plugin';
+export * from './table/duckdb-table';
 export * from './table/duckdb-table-utils';
-export default keplerGlDuckDBPlugin;
-export {
-  consolidateFieldTypes,
-  extendFieldsWithDuckDBColumnType,
-  fieldTypeToColumnType
-} from './processors/data-processor';
+export * from './processors/data-processor';
