a295172223cd5dac1ce37e5f170c2f6c4905228e
bdjulbic <bdjulbic@foursquare.com>
2026-03-15 02:11:39 +0100

    add test
    Signed-off-by: bdjulbic <bdjulbic@foursquare.com>

--- test/browser/components/modals/export-image-modal-test.js
+++ test/browser/components/modals/export-image-modal-test.js
@@ -140,6 +140,238 @@ test('Components -> ExportImageModal.preview image', t => {
   t.end();
 });
 
+test('Components -> ExportImageModal.4:3 ratio dropdown', t => {
+  const onUpdateImageSetting = sinon.spy();
+  const cleanupExportImage = () => {};
+  
+  const exportImage = {
+    ...INITIAL_UI_STATE.exportImage,
+    ratio: 'FOUR_BY_THREE',
+    resolution: '1280x960'
+  };
+  
+  let wrapper;
+  t.doesNotThrow(() => {
+    wrapper = mountWithTheme(
+      <IntlWrapper>
+        <ExportImageModal
+          mapW={500}
+          mapH={500}
+          exportImage={exportImage}
+          onUpdateImageSetting={onUpdateImageSetting}
+          cleanupExportImage={cleanupExportImage}
+        />
+      </IntlWrapper>
+    );
+  }, 'Should not fail with 4:3 ratio');
+
+  // Check that dropdown is rendered instead of buttons for 4:3 ratio
+  const dropdown = wrapper.find('#export-image-modal__option_resolution select');
+  t.equal(dropdown.length, 1, 'should render resolution dropdown for 4:3 ratio');
+  
+  // Check dropdown options
+  const options = dropdown.find('option');
+  const optionValues = options.map(opt => opt.prop('value'));
+  const optionTexts = options.map(opt => opt.text());
+  
+  t.equal(options.length, 5, 'should have 5 options (placeholder + 4 resolutions)');
+  t.equal(optionValues[0], '', 'first option should be empty placeholder');
+  t.deepEqual(
+    optionValues.slice(1),
+    ['1024x768', '1280x960', '1600x1200', '1920x1440'],
+    'should have correct 4:3 resolution values'
+  );
+  
+  // Check selected value
+  t.equal(dropdown.prop('value'), '1280x960', 'should have correct selected value');
+  
+  // Test selecting a different resolution
+  onUpdateImageSetting.resetHistory();
+  dropdown.simulate('change', { target: { value: '1600x1200' } });
+  
+  t.ok(
+    onUpdateImageSetting.calledWith({resolution: '1600x1200'}),
+    'should call onUpdateImageSetting with new resolution'
+  );
+  
+  // Test selecting empty placeholder - should select first available
+  onUpdateImageSetting.resetHistory();
+  dropdown.simulate('change', { target: { value: '' } });
+  
+  t.ok(
+    onUpdateImageSetting.calledWith({resolution: '1024x768'}),
+    'should select first available resolution when empty is selected'
+  );
+  
+  // Test selecting invalid value - should not update
+  onUpdateImageSetting.resetHistory();
+  dropdown.simulate('change', { target: { value: 'invalid-resolution' } });
+  
+  t.ok(
+    onUpdateImageSetting.notCalled,
+    'should not call onUpdateImageSetting with invalid resolution'
+  );
+  
+  // Test selecting a value not in the filtered list (e.g., 16:9 resolution)
+  onUpdateImageSetting.resetHistory();
+  dropdown.simulate('change', { target: { value: '1920x1080' } });
+  
+  t.ok(
+    onUpdateImageSetting.notCalled,
+    'should not call onUpdateImageSetting with resolution from different ratio'
+  );
+  
+  t.end();
+});
+
+test('Components -> ExportImageModal.16:9 ratio dropdown', t => {
+  const onUpdateImageSetting = sinon.spy();
+  const cleanupExportImage = () => {};
+  
+  const exportImage = {
+    ...INITIAL_UI_STATE.exportImage,
+    ratio: 'SIXTEEN_BY_NINE',
+    resolution: '1920x1080'
+  };
+  
+  let wrapper;
+  t.doesNotThrow(() => {
+    wrapper = mountWithTheme(
+      <IntlWrapper>
+        <ExportImageModal
+          mapW={500}
+          mapH={500}
+          exportImage={exportImage}
+          onUpdateImageSetting={onUpdateImageSetting}
+          cleanupExportImage={cleanupExportImage}
+        />
+      </IntlWrapper>
+    );
+  }, 'Should not fail with 16:9 ratio');
+
+  // Check that dropdown is rendered for 16:9 ratio
+  const dropdown = wrapper.find('#export-image-modal__option_resolution select');
+  t.equal(dropdown.length, 1, 'should render resolution dropdown for 16:9 ratio');
+  
+  // Check dropdown options
+  const options = dropdown.find('option');
+  const optionValues = options.map(opt => opt.prop('value'));
+  
+  t.equal(options.length, 5, 'should have 5 options (placeholder + 4 resolutions)');
+  t.equal(optionValues[0], '', 'first option should be empty placeholder');
+  t.deepEqual(
+    optionValues.slice(1),
+    ['1280x720', '1600x900', '1920x1080', '2560x1440'],
+    'should have correct 16:9 resolution values'
+  );
+  
+  // Check selected value
+  t.equal(dropdown.prop('value'), '1920x1080', 'should have correct selected value');
+  
+  // Test disabled state for unavailable options
+  const disabledStates = options.map(opt => opt.prop('disabled'));
+  t.deepEqual(
+    disabledStates,
+    [false, false, false, false, false],
+    'all options should be enabled by default'
+  );
+  
+  // Test selecting a different resolution
+  onUpdateImageSetting.resetHistory();
+  dropdown.simulate('change', { target: { value: '2560x1440' } });
+  
+  t.ok(
+    onUpdateImageSetting.calledWith({resolution: '2560x1440'}),
+    'should call onUpdateImageSetting with new resolution'
+  );
+  
+  t.end();
+});
+
+test('Components -> ExportImageModal.ratio switching', t => {
+  const onUpdateImageSetting = sinon.spy();
+  const cleanupExportImage = () => {};
+  
+  let exportImage = {
+    ...INITIAL_UI_STATE.exportImage,
+    ratio: 'SCREEN',
+    resolution: 'ONE_X'
+  };
+  
+  let wrapper;
+  t.doesNotThrow(() => {
+    wrapper = mountWithTheme(
+      <IntlWrapper>
+        <ExportImageModal
+          mapW={500}
+          mapH={500}
+          exportImage={exportImage}
+          onUpdateImageSetting={onUpdateImageSetting}
+          cleanupExportImage={cleanupExportImage}
+        />
+      </IntlWrapper>
+    );
+  }, 'Should not fail with SCREEN ratio');
+
+  // Initially should show buttons for SCREEN ratio
+  t.equal(
+    wrapper.find('#export-image-modal__option_resolution').find(SelectionButton).length,
+    2,
+    'should show resolution buttons for SCREEN ratio'
+  );
+  t.equal(
+    wrapper.find('#export-image-modal__option_resolution select').length,
+    0,
+    'should not show dropdown for SCREEN ratio'
+  );
+  
+  // Switch to 4:3 ratio
+  onUpdateImageSetting.resetHistory();
+  wrapper
+    .find('#export-image-modal__option_ratio')
+    .find(SelectionButton)
+    .at(1) // 4:3 is the second option
+    .simulate('click');
+    
+  t.ok(
+    onUpdateImageSetting.calledWith({ratio: 'FOUR_BY_THREE'}),
+    'should call onUpdateImageSetting with 4:3 ratio'
+  );
+  
+  // Update wrapper with new ratio
+  exportImage = {
+    ...exportImage,
+    ratio: 'FOUR_BY_THREE',
+    resolution: '1280x960'
+  };
+  wrapper.setProps({
+    children: (
+      <ExportImageModal
+        mapW={500}
+        mapH={500}
+        exportImage={exportImage}
+        onUpdateImageSetting={onUpdateImageSetting}
+        cleanupExportImage={cleanupExportImage}
+      />
+    )
+  });
+  wrapper.update();
+  
+  // Should now show dropdown for 4:3 ratio
+  t.equal(
+    wrapper.find('#export-image-modal__option_resolution select').length,
+    1,
+    'should show dropdown for 4:3 ratio'
+  );
+  t.equal(
+    wrapper.find('#export-image-modal__option_resolution').find(SelectionButton).length,
+    0,
+    'should not show buttons for 4:3 ratio'
+  );
+  
+  t.end();
+});
+
 test('Components -> ExportImageModal.unmount', t => {
   const onUpdateImageSetting = () => {};
   const cleanupExportImage = sinon.spy();
