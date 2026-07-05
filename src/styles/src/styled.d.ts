14cf427eabca59de1431977bda53af398954d777
Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>
2026-07-06 01:35:45 +0300

    bump styled components
    Signed-off-by: Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>

---
+++ src/styles/src/styled.d.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the kepler.gl project
+
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    [key: string]: any;
+  }
+}
