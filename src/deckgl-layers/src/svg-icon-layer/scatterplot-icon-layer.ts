f7a31330339bd25e3ee9296b512ab2a69c9dc4da
Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>
2026-07-19 16:00:11 +0300

    icon layer fixes
    Signed-off-by: Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>

--- src/deckgl-layers/src/svg-icon-layer/scatterplot-icon-layer.ts
+++ src/deckgl-layers/src/svg-icon-layer/scatterplot-icon-layer.ts
@@ -2,10 +2,38 @@
 // Copyright contributors to the kepler.gl project
 
 import {ScatterplotLayer, ScatterplotLayerProps} from '@deck.gl/layers';
-import {Geometry} from '@luma.gl/engine';
+import {Geometry, Model} from '@luma.gl/engine';
 import {TOPOLOGY} from '@kepler.gl/constants';
 
 const DEFAULT_POS = [-1, -1, 0, -1, 1, 0, 1, 1, 0, 1, -1, 0];
+
+const globeModeUniforms = {
+  name: 'globeMode',
+  vs: `uniform globeModeUniforms {
+  float globeModeMod;
+} globeModeProps;
+`,
+  uniformTypes: {
+    globeModeMod: 'f32'
+  }
+};
+
+const iconFragmentShader = /* glsl */ `\
+#version 300 es
+#define SHADER_NAME scatterplot-icon-layer-fragment-shader
+precision highp float;
+in vec4 vFillColor;
+in vec4 vLineColor;
+in vec2 unitPosition;
+in float innerUnitRadius;
+in float outerRadiusPixels;
+out vec4 fragColor;
+void main(void) {
+  geometry.uv = unitPosition;
+  fragColor = vFillColor;
+  DECKGL_FILTER_COLOR(fragColor, geometry);
+}
+`;
 
 export interface ScatterplotIconLayerProps extends ScatterplotLayerProps<any> {
   iconGeometry: number[];
@@ -13,20 +41,27 @@ export interface ScatterplotIconLayerProps extends ScatterplotLayerProps<any> {
 
 export default class ScatterplotIconLayer extends ScatterplotLayer<any, ScatterplotIconLayerProps> {
   getShaders() {
+    const shaders = super.getShaders();
     return {
-      ...super.getShaders(),
+      ...shaders,
+      fs: iconFragmentShader,
+      modules: [...(shaders.modules || []), globeModeUniforms],
       inject: {
-        'vs:#decl': `
-          uniform float u_globeModeMod;`,
         'vs:DECKGL_FILTER_SIZE': `
-          size.xy *= u_globeModeMod;`
+          size.xy *= globeModeProps.globeModeMod;`
       }
     };
   }
 
   draw(opts) {
-    opts.uniforms = opts.uniforms || {};
-    opts.uniforms.u_globeModeMod = (this.context.viewport as any).resolution ? -0.5 : 1;
+    const model = this.state.model;
+    if (model) {
+      model.shaderInputs.setProps({
+        globeMode: {
+          globeModeMod: (this.context.viewport as any).resolution ? -0.5 : 1
+        }
+      });
+    }
     super.draw(opts);
   }
 
@@ -34,18 +69,18 @@ export default class ScatterplotIconLayer extends ScatterplotLayer<any, Scatterp
     const {iconGeometry} = this.props;
     const positions = iconGeometry ? new Float32Array(iconGeometry) : new Float32Array(DEFAULT_POS);
 
-    const geometry = new Geometry({
-      topology: iconGeometry ? TOPOLOGY.TRIANGLE_LIST : TOPOLOGY.TRIANGLE_STRIP,
-      attributes: {
-        positions: {size: 3, value: positions}
-      }
+    return new Model(this.context.device, {
+      ...this.getShaders(),
+      id: this.props.id,
+      bufferLayout: this.getAttributeManager()!.getBufferLayouts(),
+      geometry: new Geometry({
+        topology: iconGeometry ? TOPOLOGY.TRIANGLE_LIST : TOPOLOGY.TRIANGLE_STRIP,
+        attributes: {
+          positions: {size: 3, value: positions}
+        }
+      }),
+      isInstanced: true
     });
-
-    const model = super._getModel();
-    if (model) {
-      model.setGeometry(geometry);
-    }
-    return model;
   }
 }
 
