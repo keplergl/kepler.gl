885d213ccc4e6a65e6a16a434f7ef32bedca00a3
Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>
2026-07-31 05:42:13 +0300

    lint
    Signed-off-by: Ihor Dykhta <ihordykhta@Ihors-MacBook-Pro.local>

--- src/layers/src/raster-tile/raster-tile-layer.ts
+++ src/layers/src/raster-tile/raster-tile-layer.ts
@@ -2,7 +2,7 @@
 // Copyright contributors to the kepler.gl project
 
 import {COORDINATE_SYSTEM, Layer as DeckLayer} from '@deck.gl/core';
-import {TileLayer, GeoBoundingBox} from '@deck.gl/geo-layers';
+import {TileLayer, GeoBoundingBox, TileBoundingBox} from '@deck.gl/geo-layers';
 import {PMTilesSource, PMTilesTileSource} from '@loaders.gl/pmtiles';
 import type {TypedArray} from '@loaders.gl/loader-utils';
 import type {TextureProps} from '@luma.gl/core';
@@ -877,7 +877,7 @@ export default class RasterTileLayer extends KeplerLayer {
  */
 function buildTileDebugLayer(
   id: string,
-  tile: {index: {x: number; y: number; z: number}; bbox: GeoBoundingBox},
+  tile: {index: {x: number; y: number; z: number}; bbox: TileBoundingBox},
   status: string,
   extra: string
 ): TextLayer {
